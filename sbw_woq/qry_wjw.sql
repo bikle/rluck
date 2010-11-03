1f0037248c24fd0c3868dbe993d2e7821d650795
Dan Bikle <bikle@bikle.com>
2010-11-03 00:35:23 -0700

    c

--- sbw_woq/qry_wjw.sql
+++ sbw_woq/qry_wjw.sql
@@ -11,8 +11,46 @@
 -- I can start week of quarter (woq) on any day of the week.
 -- If I start woq on a Tues, then woq_start_day is 2.
 
--- Start by comparing gains before 2009 to gains after 2009:
-CREATE OR REPLACE VIEW qwjw10 AS
+-- Start by comparing gains before 2007 to gains after 2007:
+CREATE OR REPLACE VIEW qwjw2007 AS
+SELECT
+a.pair
+,a.woq
+,a.day_of_week woq_start_day
+,AVG(a.wgain)avg_a
+,AVG(b.wgain)avg_b
+,COUNT(a.pair)row_cnt
+FROM
+  (SELECT pair,woq,day_of_week,wgain FROM wjw WHERE ydate2<'2007-01-01')a
+ ,(SELECT pair,woq,day_of_week,wgain FROM wjw WHERE ydate1>'2007-01-01')b
+WHERE a.pair = b.pair AND a.woq = b.woq
+GROUP BY a.pair,a.woq,a.day_of_week
+-- If the pair count is low, it is noise:
+HAVING COUNT(a.pair)> 123
+ORDER BY a.pair,a.woq,a.day_of_week
+/
+
+-- 2008 is next:
+CREATE OR REPLACE VIEW qwjw2008 AS
+SELECT
+a.pair
+,a.woq
+,a.day_of_week woq_start_day
+,AVG(a.wgain)avg_a
+,AVG(b.wgain)avg_b
+,COUNT(a.pair)row_cnt
+FROM
+  (SELECT pair,woq,day_of_week,wgain FROM wjw WHERE ydate2<'2008-01-01')a
+ ,(SELECT pair,woq,day_of_week,wgain FROM wjw WHERE ydate1>'2008-01-01')b
+WHERE a.pair = b.pair AND a.woq = b.woq
+GROUP BY a.pair,a.woq,a.day_of_week
+-- If the pair count is low, it is noise:
+HAVING COUNT(a.pair)> 123
+ORDER BY a.pair,a.woq,a.day_of_week
+/
+
+-- 2009 is next
+CREATE OR REPLACE VIEW qwjw2009 AS
 SELECT
 a.pair
 ,a.woq
@@ -30,12 +68,121 @@ HAVING COUNT(a.pair)> 123
 ORDER BY a.pair,a.woq,a.day_of_week
 /
 
--- rpt
+-- 2010 is next
+CREATE OR REPLACE VIEW qwjw2010 AS
+SELECT
+a.pair
+,a.woq
+,a.day_of_week woq_start_day
+,AVG(a.wgain)avg_a
+,AVG(b.wgain)avg_b
+,COUNT(a.pair)row_cnt
+FROM
+  (SELECT pair,woq,day_of_week,wgain FROM wjw WHERE ydate2<'2010-01-01')a
+ ,(SELECT pair,woq,day_of_week,wgain FROM wjw WHERE ydate1>'2010-01-01')b
+WHERE a.pair = b.pair AND a.woq = b.woq
+GROUP BY a.pair,a.woq,a.day_of_week
+-- If the pair count is low, it is noise:
+HAVING COUNT(a.pair)> 123
+ORDER BY a.pair,a.woq,a.day_of_week
+/
+
+-- I now look at 2007,8,9,10:
 SELECT pair,COUNT(pair),CORR(avg_a,avg_b)
-FROM qwjw10
+FROM qwjw2007
 GROUP BY pair
 HAVING CORR(avg_a,avg_b)>0
 ORDER BY CORR(avg_a,avg_b)
+/
+
+SELECT pair,COUNT(pair),CORR(avg_a,avg_b)
+FROM qwjw2008
+GROUP BY pair
+HAVING CORR(avg_a,avg_b)>0
+ORDER BY CORR(avg_a,avg_b)
+/
+
+SELECT pair,COUNT(pair),CORR(avg_a,avg_b)
+FROM qwjw2009
+GROUP BY pair
+HAVING CORR(avg_a,avg_b)>0
+ORDER BY CORR(avg_a,avg_b)
+/
+
+SELECT pair,COUNT(pair),CORR(avg_a,avg_b)
+FROM qwjw2010
+GROUP BY pair
+HAVING CORR(avg_a,avg_b)>0
+ORDER BY CORR(avg_a,avg_b)
+/
+
+
+-- usd_chf is the only pair to exhibit a correlation trend for all 4 years.
+
+-- I look more closely at usd_chf:
+
+SELECT * FROM
+(
+  SELECT
+  woq_start_day
+  ,woq
+  ,avg_a
+  ,avg_b
+  ,row_cnt
+  FROM qwjw2007
+  WHERE avg_a * avg_b > 0
+  AND pair = 'usd_chf'
+  ORDER BY(avg_a * avg_b)DESC
+)
+WHERE rownum < 10
+/
+
+SELECT * FROM
+(
+  SELECT
+  woq_start_day
+  ,woq
+  ,avg_a
+  ,avg_b
+  ,row_cnt
+  FROM qwjw2008
+  WHERE avg_a * avg_b > 0
+  AND pair = 'usd_chf'
+  ORDER BY(avg_a * avg_b)DESC
+)
+WHERE rownum < 10
+/
+
+SELECT * FROM
+(
+  SELECT
+  woq_start_day
+  ,woq
+  ,avg_a
+  ,avg_b
+  ,row_cnt
+  FROM qwjw2009
+  WHERE avg_a * avg_b > 0
+  AND pair = 'usd_chf'
+  ORDER BY(avg_a * avg_b)DESC
+)
+WHERE rownum < 10
+/
+
+SELECT * FROM
+(
+  SELECT
+  woq_start_day
+  ,woq
+  ,avg_a
+  ,avg_b
+  ,row_cnt
+  FROM qwjw2010
+  WHERE avg_a * avg_b > 0
+  AND pair = 'usd_chf'
+  ORDER BY(avg_a * avg_b)DESC
+)
+WHERE rownum < 10
 /
 
 
