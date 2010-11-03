d0030698653acdcea67a2b15f9db5c22d794c9a4
Dan Bikle <bikle@bikle.com>
2010-11-03 01:20:19 -0700

    c

--- sbw_woq/qry_wjw.sql
+++ sbw_woq/qry_wjw.sql
@@ -15,76 +15,76 @@
 CREATE OR REPLACE VIEW qwjw2007 AS
 SELECT
 a.pair
-,a.woq,a.qtr
+,a.woq,b.qtr
 ,a.day_of_week woq_start_day
 ,AVG(a.wgain)avg_a
 ,AVG(b.wgain)avg_b
 ,COUNT(a.pair)row_cnt
 FROM
-  (SELECT pair,woq,qtr,day_of_week,wgain FROM wjw WHERE ydate2<'2007-01-01')a
+  (SELECT pair,woq,qtr,day_of_week,wgain FROM wjw WHERE ydate1<'2007-01-01')a
  ,(SELECT pair,woq,qtr,day_of_week,wgain FROM wjw WHERE ydate1>'2007-01-01')b
 WHERE a.pair = b.pair AND a.woq = b.woq
-GROUP BY a.pair,a.woq,a.qtr,a.day_of_week
+GROUP BY a.pair,a.woq,b.qtr,a.day_of_week
 -- If the pair count is low, it is noise:
 HAVING COUNT(a.pair)> 123
-ORDER BY a.pair,a.woq,a.qtr,a.day_of_week
+ORDER BY a.pair,a.woq,b.qtr,a.day_of_week
 /
 
 -- 2008 is next:
 CREATE OR REPLACE VIEW qwjw2008 AS
 SELECT
 a.pair
-,a.woq,a.qtr
+,a.woq,b.qtr
 ,a.day_of_week woq_start_day
 ,AVG(a.wgain)avg_a
 ,AVG(b.wgain)avg_b
 ,COUNT(a.pair)row_cnt
 FROM
-  (SELECT pair,woq,qtr,day_of_week,wgain FROM wjw WHERE ydate2<'2008-01-01')a
+  (SELECT pair,woq,qtr,day_of_week,wgain FROM wjw WHERE ydate1<'2008-01-01')a
  ,(SELECT pair,woq,qtr,day_of_week,wgain FROM wjw WHERE ydate1>'2008-01-01')b
 WHERE a.pair = b.pair AND a.woq = b.woq
-GROUP BY a.pair,a.woq,a.qtr,a.day_of_week
+GROUP BY a.pair,a.woq,b.qtr,a.day_of_week
 -- If the pair count is low, it is noise:
 HAVING COUNT(a.pair)> 123
-ORDER BY a.pair,a.woq,a.qtr,a.day_of_week
+ORDER BY a.pair,a.woq,b.qtr,a.day_of_week
 /
 
 -- 2009 is next
 CREATE OR REPLACE VIEW qwjw2009 AS
 SELECT
 a.pair
-,a.woq,a.qtr
+,a.woq,b.qtr
 ,a.day_of_week woq_start_day
 ,AVG(a.wgain)avg_a
 ,AVG(b.wgain)avg_b
 ,COUNT(a.pair)row_cnt
 FROM
-  (SELECT pair,woq,qtr,day_of_week,wgain FROM wjw WHERE ydate2<'2009-01-01')a
+  (SELECT pair,woq,qtr,day_of_week,wgain FROM wjw WHERE ydate1<'2009-01-01')a
  ,(SELECT pair,woq,qtr,day_of_week,wgain FROM wjw WHERE ydate1>'2009-01-01')b
 WHERE a.pair = b.pair AND a.woq = b.woq
-GROUP BY a.pair,a.woq,a.qtr,a.day_of_week
+GROUP BY a.pair,a.woq,b.qtr,a.day_of_week
 -- If the pair count is low, it is noise:
 HAVING COUNT(a.pair)> 123
-ORDER BY a.pair,a.woq,a.qtr,a.day_of_week
+ORDER BY a.pair,a.woq,b.qtr,a.day_of_week
 /
 
 -- 2010 is next
 CREATE OR REPLACE VIEW qwjw2010 AS
 SELECT
 a.pair
-,a.woq,a.qtr
+,a.woq,b.qtr
 ,a.day_of_week woq_start_day
 ,AVG(a.wgain)avg_a
 ,AVG(b.wgain)avg_b
 ,COUNT(a.pair)row_cnt
 FROM
-  (SELECT pair,woq,qtr,day_of_week,wgain FROM wjw WHERE ydate2<'2010-01-01')a
+  (SELECT pair,woq,qtr,day_of_week,wgain FROM wjw WHERE ydate1<'2010-01-01')a
  ,(SELECT pair,woq,qtr,day_of_week,wgain FROM wjw WHERE ydate1>'2010-01-01')b
 WHERE a.pair = b.pair AND a.woq = b.woq
-GROUP BY a.pair,a.woq,a.qtr,a.day_of_week
+GROUP BY a.pair,a.woq,b.qtr,a.day_of_week
 -- If the pair count is low, it is noise:
 HAVING COUNT(a.pair)> 123
-ORDER BY a.pair,a.woq,a.qtr,a.day_of_week
+ORDER BY a.pair,a.woq,b.qtr,a.day_of_week
 /
 
 -- I now look at 2007,8,9,10:
@@ -254,6 +254,40 @@ SELECT * FROM
 WHERE rownum < 10
 /
 
+-- Now look for a woq-correlation-trend which covers usd_chf, 2009, 2010:
+
+CREATE OR REPLACE VIEW qwjw910 AS
+SELECT
+a.pair
+,a.woq,b.qtr
+,a.day_of_week woq_start_day
+,AVG(a.wgain)avg_a
+,AVG(b.wgain)avg_b
+,COUNT(a.pair)row_cnt
+FROM
+  (SELECT pair,woq,qtr,day_of_week,wgain FROM wjw WHERE ydate1 BETWEEN'2009-01-01'AND'2009-12-31')a
+ ,(SELECT pair,woq,qtr,day_of_week,wgain FROM wjw WHERE ydate1 BETWEEN'2010-01-01'AND'2010-12-31')b
+WHERE a.pair = b.pair AND a.woq = b.woq
+GROUP BY a.pair,a.woq,b.qtr,a.day_of_week
+-- If the pair count is low, it is noise:
+ORDER BY a.pair,a.woq,b.qtr,a.day_of_week
+/
+
+SELECT * FROM
+(
+  SELECT
+  qtr
+  ,woq
+  ,avg_a
+  ,avg_b
+  ,row_cnt
+  FROM qwjw910
+  WHERE avg_a * avg_b > 0
+  AND pair = 'usd_chf'
+  ORDER BY(avg_a * avg_b)DESC
+)
+WHERE rownum < 10
+/
 
 exit
 
