1c7c1f0875b2926e584a1b8ba54c6e3e44046c5e
Dan Bikle <bikle@bikle.com>
2010-11-09 07:38:51 +0000

    6hp

---
+++ 6hp2010_1108/6hp.sql
@@ -0,0 +1,79 @@
+--
+-- 6hp.sql
+--
+
+SET LINES 66
+DESC hourly
+SET LINES 166
+
+CREATE OR REPLACE VIEW hp10 AS
+SELECT
+pair
+-- ydate is granular down to the hour:
+,ydate
+,opn
+-- Derive an attribute I call "day_hour":
+,TO_CHAR(ydate,'D')||'_'||TO_CHAR(ydate,'HH24')dhr
+-- Get ydate 6 hours in the future:
+,LEAD(ydate,6,NULL)OVER(PARTITION BY pair ORDER BY pair,ydate) ydate6
+-- Get closing price 6 hours in the future:
+,LEAD(clse,5,NULL)OVER(PARTITION BY pair ORDER BY pair,ydate) clse6
+FROM hourly 
+WHERE ydate > '2009-01-01'
+-- Prevent divide by zero:
+AND opn > 0
+ORDER BY pair,ydate
+/
+
+-- I derive more attributes:
+CREATE OR REPLACE VIEW hp12 AS
+SELECT
+pair
+,ydate
+,opn
+,dhr
+,ydate6
+,clse6
+,(clse6 - opn)/opn npg
+FROM hp10
+ORDER BY pair,ydate
+/
+
+--rpt
+SELECT COUNT(ydate)FROM hp10;
+SELECT AVG(ydate6 - ydate), MIN(ydate6 - ydate),MAX(ydate6 - ydate),COUNT(ydate)FROM hp12;
+-- I should see no rows WHERE the date difference is less than 6 hours:
+SELECT COUNT(ydate)FROM hp12 WHERE (ydate6 - ydate) < 6/24;
+
+-- I should see many rows WHERE the date difference is exactly 6 hours:
+SELECT COUNT(ydate)FROM hp12 WHERE (ydate6 - ydate) = 6/24
+
+-- I should see some rows 
+-- WHERE the date difference is greater than 6 hours due to Saturday getting sandwiched between some of the records.
+-- Also if I am missing some rows, counts will appear here:
+SELECT TO_CHAR(ydate,'Day'),MIN(ydate),COUNT(ydate),MAX(ydate)
+FROM hp12 WHERE (ydate6 - ydate) > 6/24
+GROUP BY TO_CHAR(ydate,'Day')
+ORDER BY COUNT(ydate)
+/
+
+-- Now I can aggregate:
+SELECT
+pair,dhr
+,COUNT(npg)count_npg
+,ROUND(MIN(npg),4)min_npg
+,ROUND(AVG(npg),4)avg_npg
+,ROUND(STDDEV(npg),4)stddev_npg
+,ROUND(MAX(npg),4)max_npg
+,ROUND(SUM(npg),4)sum_npg
+FROM hdp
+WHERE (ydate6 - ydate) = 6/24
+GROUP BY pair,dhr
+-- I want more than 1 pip / hr which is 6 pips:
+HAVING AVG(npg) > 0.0006
+ORDER BY pair,dhr
+/
+
+
+exit
+
