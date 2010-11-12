622a3fff6057d956d1554903d117186fd00921e7
Dan Bikle <bikle@bikle.com>
2010-11-12 01:39:11 +0000

    svm6

--- svm6/build_scorem.sql
+++ svm6/build_scorem.sql
@@ -5,6 +5,7 @@
 -- A simple SQL script I use to help me build another SQL script full of calls to the scoring script.
 SET HEADING OFF
 
+-- I only want to score rows from 2010:
 SELECT '@score1hr.sql ',ydate FROM svm6ms WHERE ydate > '2010-01-01'ORDER BY ydate;
 
 exit
--- svm6/svm6.sql
+++ svm6/svm6.sql
@@ -12,9 +12,9 @@ ydate
 -- 2nd SVM attribute:
 ,0+TO_CHAR(ydate,'HH24')hr
 -- Get ydate 6 hours in the future:
-,LEAD(ydate,6,NULL)OVER(ORDER BY ydate) ydate6
+,LEAD(ydate,6,NULL)OVER(PARTITION BY pair ORDER BY ydate) ydate6
 -- Get closing price 6 rows (usually 6 hours) in the future:
-,LEAD(clse,5,NULL)OVER(ORDER BY ydate) clse6
+,LEAD(clse,5,NULL)OVER(PARTITION BY pair ORDER BY ydate) clse6
 FROM hourly 
 WHERE ydate >'2009-01-01'
 -- Prevent divide by zero:
@@ -23,6 +23,10 @@ AND opn > 0
 AND pair = 'aud_usd'
 ORDER BY ydate
 /
+
+SELECT COUNT(*)FROM svm610 WHERE ydate6 IS NULL;
+SELECT COUNT(*)FROM svm610 WHERE clse6 IS NULL;
+
 
 -- I derive more attributes:
 CREATE OR REPLACE VIEW svm6ms AS
@@ -34,11 +38,24 @@ ydate
 ,ydate6
 ,clse6
 ,(clse6 - opn)/opn npg
+-- I use a simple CASE expression to build gatt which is the target attribute.
+-- Notice that gatt can have 3 values,
+-- NULL, 'up', and 'nup'.
+-- To me, 'nup' means not-up rather than down:
+,CASE WHEN clse6 IS NULL THEN NULL 
+      WHEN (clse6 - opn)/opn > 0.0009 THEN 'up'
+      ELSE 'nup' END gatt
 FROM svm610
 ORDER BY ydate
 /
 
 --rpt
+
+
+SELECT COUNT(*)FROM svm6ms WHERE ydate6 IS NULL;
+SELECT COUNT(*)FROM svm6ms WHERE clse6 IS NULL;
+SELECT COUNT(*)FROM svm6ms WHERE gatt IS NULL;
+
 SELECT COUNT(ydate)FROM svm610;
 
 SELECT COUNT(ydate),AVG(ydate6-ydate),MIN(ydate6-ydate),MAX(ydate6-ydate) FROM svm6ms;
@@ -48,6 +65,10 @@ SELECT COUNT(ydate)FROM svm6ms WHERE (ydate6 - ydate) < 6/24;
 
 -- I should see many rows WHERE the date difference is exactly 6 hours:
 SELECT COUNT(ydate)FROM svm6ms WHERE (ydate6 - ydate) = 6/24;
+
+-- I show up, nup distribution.
+-- I should see 5 rows that are too far in the future to give me a gatt value yet:
+SELECT NVL(gatt,'null_gatt')gatt,COUNT(NVL(gatt,'null_gatt'))FROM svm6ms GROUP BY NVL(gatt,'null_gatt');
 
 exit
 
