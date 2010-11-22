fa8692f20ea59e2b7d157ae6fd8c429c415a57e1
Dan Bikle <bikle@bikle.com>
2010-11-22 23:02:19 +0000

    4hp

--- 4hp2010_1118/4hp.sql
+++ 4hp2010_1118/4hp.sql
@@ -5,7 +5,6 @@
 SET LINES 66
 -- I create h15c using h15c.sql to derive prices from hourly table:
 DESC h15c
-SELECT pair,MIN(ydate),COUNT(*),MAX(ydate)FROM h15c GROUP BY pair;
 SET LINES 166
 
 CREATE OR REPLACE VIEW hp10 AS
@@ -26,20 +25,19 @@ pair
 -- FROM hourly 
 -- I use h15c to supply 15 pairs instead of just 6:
 FROM h15c
-WHERE ydate > '2009-01-01'
+WHERE ydate > sysdate - 366
 -- Prevent divide by zero:
 AND clse > 0
 ORDER BY pair,ydate
 /
 
--- I create a view of what I call "Near-Past":
-CREATE OR REPLACE VIEW hpnp AS SELECT * FROM hp10 WHERE ydate > sysdate - 60;
+-- hp10 rpt:
+-- I should see 15 pairs and nearly identical counts for each pair:
+SELECT pair,MIN(ydate),COUNT(*),MAX(ydate)FROM hp10 GROUP BY pair ORDER BY pair;
 
--- I create a view of what I call "Far-Past":
-CREATE OR REPLACE VIEW hpfp AS SELECT * FROM hp10 WHERE ydate <= sysdate - 60;
-
---rpt
+-- Study distribution of (ydate4 - ydate):
 SELECT AVG(ydate4 - ydate), MIN(ydate4 - ydate),MAX(ydate4 - ydate),COUNT(ydate)FROM hp10;
+
 -- I should see no rows WHERE the date difference is less than 4 hours:
 SELECT COUNT(ydate)FROM hp10 WHERE (ydate4 - ydate) < 4/24;
 
@@ -54,6 +52,13 @@ FROM hp10 WHERE (ydate4 - ydate) > 4/24
 GROUP BY TO_CHAR(ydate,'Day')
 ORDER BY COUNT(ydate)
 /
+
+-- I create a view of what I call "Near-Past":
+CREATE OR REPLACE VIEW hpnp AS SELECT * FROM hp10 WHERE ydate > sysdate - 60;
+
+-- I create a view of what I call "Far-Past":
+CREATE OR REPLACE VIEW hpfp AS SELECT * FROM hp10 WHERE ydate <= sysdate - 60;
+
 
 -- Now I can aggregate Far-Past:
 CREATE OR REPLACE VIEW hpfp_agg AS
@@ -72,7 +77,7 @@ GROUP BY pair,dhr
 HAVING ABS(AVG(npg)) > 1.5 * 0.0001 * 4
 /
 
--- Sort by pair 1st:
+-- hpfp_agg rpt:
 SELECT COUNT(*)FROM hpfp_agg;
 
 SELECT * FROM hpfp_agg ORDER BY pair,dhr
@@ -92,6 +97,7 @@ WHERE (ydate4 - ydate) = 4/24
 GROUP BY pair,dhr
 /
 
+-- hpnp_agg rpt:
 SELECT COUNT(*)FROM hpnp_agg;
 
 SELECT * FROM hpnp_agg ORDER BY pair,dhr
@@ -111,8 +117,34 @@ WHERE a.pair = b.pair AND a.dhr = b.dhr
 -- Sort by dhr since I can then easily compare to my calendar:
 SELECT * FROM hp4aggj ORDER BY dhr,pair;
 
--- Look for CORR():
-SELECT pair, CORR(avg_npg1,avg_npg2),COUNT(*)FROM hp4aggj GROUP BY pair ORDER BY pair;
+-- Look for CORR() for each pair:
+SELECT pair, CORR(avg_npg1,avg_npg2),COUNT(dhr)FROM hp4aggj GROUP BY pair ORDER BY pair;
+
+-- Look CORR() for each day:
+SELECT CORR(avg_npg1,avg_npg2),COUNT(dhr)FROM hp4aggj WHERE dhr LIKE'1%';
+SELECT CORR(avg_npg1,avg_npg2),COUNT(dhr)FROM hp4aggj WHERE dhr LIKE'2%';
+SELECT CORR(avg_npg1,avg_npg2),COUNT(dhr)FROM hp4aggj WHERE dhr LIKE'3%';
+SELECT CORR(avg_npg1,avg_npg2),COUNT(dhr)FROM hp4aggj WHERE dhr LIKE'4%';
+SELECT CORR(avg_npg1,avg_npg2),COUNT(dhr)FROM hp4aggj WHERE dhr LIKE'5%';
+SELECT CORR(avg_npg1,avg_npg2),COUNT(dhr)FROM hp4aggj WHERE dhr LIKE'6%';
+
+-- Look at CORR()-combo of some pairs and days:
+
+SELECT CORR(avg_npg1,avg_npg2),COUNT(dhr)FROM hp4aggj WHERE dhr LIKE'3%'AND pair IN('gbp_usd','gbp_chf');
+
+SELECT
+CORR(avg_npg1,avg_npg2)
+,COUNT(dhr)
+FROM hp4aggj
+WHERE dhr LIKE'4%'
+AND pair IN('aud_usd','eur_aud','gbp_jpy','usd_jpy')
+/
+
+-- Look at pair IN('gbp_usd','gbp_chf') WHERE dhr LIKE'3%':
+SELECT * FROM hp4aggj WHERE dhr LIKE'3%'AND pair IN('gbp_usd','gbp_chf')ORDER BY dhr,pair;
+
+-- Look at pair IN('aud_usd','eur_aud','gbp_jpy','usd_jpy') WHERE dhr LIKE'4%':
+SELECT * FROM hp4aggj WHERE dhr LIKE'4%'AND pair IN('aud_usd','eur_aud','gbp_jpy','usd_jpy')ORDER BY dhr,pair;
 
 exit
 
