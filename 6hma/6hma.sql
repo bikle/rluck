91979e32bd5e23a52124d7f0f8feba57d58a6ea5
Dan Bikle <bikle@bikle.com>
2010-11-27 08:32:42 +0000

    6hma

--- 6hma/6hma.sql
+++ 6hma/6hma.sql
@@ -3,14 +3,14 @@
 --
 
 -- I use this script to help me answer the question,
--- Is moving average slope predictive?
+-- Is slope of moving average predictive?
 
 CREATE OR REPLACE VIEW v6hma10 AS
 SELECT
 pair
 -- ydate is granular down to the hour:
 ,ydate
-,opn
+,clse
 -- Use analytic function to get moving average1:
 ,AVG(clse)OVER(PARTITION BY pair ORDER BY ydate ROWS BETWEEN 7 PRECEDING AND 1 PRECEDING)ma1_6
 ,AVG(clse)OVER(PARTITION BY pair ORDER BY ydate ROWS BETWEEN 10 PRECEDING AND 1 PRECEDING)ma1_9
@@ -24,47 +24,148 @@ pair
 -- Get ydate 6 hours in the future:
 ,LEAD(ydate,6,NULL)OVER(PARTITION BY pair ORDER BY ydate) ydate6
 -- Get closing price 6 rows (usually 6 hours) in the future:
-,LEAD(clse,5,NULL)OVER(PARTITION BY pair ORDER BY ydate) clse6
+,LEAD(clse,6,NULL)OVER(PARTITION BY pair ORDER BY ydate) clse6
 FROM hourly 
-WHERE ydate >'2009-01-01'
+WHERE ydate >'2007-07-01'
 -- Prevent divide by zero:
-AND opn > 0
--- Focus on aud_usd for now:
+AND clse > 0
 ORDER BY ydate
+/
+
+-- rpt
+SELECT
+pair
+,MIN(ydate)
+,COUNT(ydate)
+,MAX(ydate)
+FROM v6hma10
+GROUP BY pair
+ORDER BY pair
 /
 
 -- I should see 6 x 6:
 SELECT pair,COUNT(*)FROM v6hma10 WHERE ydate6 IS NULL GROUP BY pair;
--- I should see 6 x 5:
 SELECT pair,COUNT(*)FROM v6hma10 WHERE clse6 IS NULL GROUP BY pair;
 
--- I derive slope of moving-averages:
-CREATE OR REPLACE VIEW v6hma AS
+-- I derive "normalized" slope of moving-averages.
+-- Also, I derive normalized gain.
+-- I normalize to prevent usd_jpy from skewing my results:
+CREATE OR REPLACE VIEW v6hma12 AS
 SELECT
 pair
 ,ydate
-,(ma2_6 - ma1_6)ma6_slope
-,(ma2_9 - ma1_9)ma9_slope
-,(ma2_12 - ma1_12)ma12_slope
-,(ma2_18 - ma1_18)ma18_slope
+,(ma2_6 - ma1_6)/ma1_6    ma6_slope
+,(ma2_9 - ma1_9)/ma1_9    ma9_slope
+,(ma2_12 - ma1_12)/ma1_12 ma12_slope
+,(ma2_18 - ma1_18)/ma1_18 ma18_slope
 ,ydate6
 ,clse6
-,(clse6 - opn)/opn npg
+,(clse6 - clse)/clse npg
 FROM v6hma10
 ORDER BY ydate
 /
 
--- Is ma_slope correlated with npg?
-SELECT 
+-- I derive moving-stddev of slope of moving-averages:
+CREATE OR REPLACE VIEW v6hma14 AS
+SELECT
 pair
-,COUNT(npg)
+,ydate
+,ma6_slope
+,ma9_slope
+,ma12_slope
+,ma18_slope
+,ydate6
+,clse6
+,npg
+,STDDEV(ma6_slope)OVER(PARTITION BY pair ORDER BY ydate ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW)stddev6
+,STDDEV(ma9_slope)OVER(PARTITION BY pair ORDER BY ydate ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW)stddev9
+,STDDEV(ma12_slope)OVER(PARTITION BY pair ORDER BY ydate ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW)stddev12
+,STDDEV(ma18_slope)OVER(PARTITION BY pair ORDER BY ydate ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW)stddev18
+FROM v6hma12
+ORDER BY ydate
+/
+
+-- I derive steepness indicator from stddev:
+CREATE OR REPLACE VIEW v6hma AS
+SELECT
+pair
+,ydate
+,ma6_slope
+,ma9_slope
+,ma12_slope
+,ma18_slope
+,ydate6
+,clse6
+,npg
+,stddev6
+,stddev9
+,stddev12
+,stddev18
+-- 1 is normal steepness, 3 is very steep (and rare):
+,CASE WHEN ABS(ma6_slope) < stddev6 THEN 1
+      WHEN ABS(ma6_slope)BETWEEN stddev6 AND 2*stddev6 THEN 2
+      WHEN ABS(ma6_slope)>2*stddev6 THEN 3 END steepness
+FROM v6hma14
+ORDER BY ydate
+/
+
+-- Now I can run some interesting queries:
+
+-- Is ma_slope correlated with npg?
+SELECT
+pair
 ,CORR(ma6_slope,npg)
-,CORR(ma9_slope,npg)
-,CORR(ma12_slope,npg)
-,CORR(ma18_slope,npg)
+,COUNT(npg)
 FROM v6hma
+WHERE steepness > 0
 GROUP BY pair
 ORDER BY pair
 /
+
+-- Does CORR() change as steepness gets large?
+SELECT
+steepness
+,pair
+,CORR(ma6_slope,npg)
+,COUNT(npg)
+FROM v6hma
+WHERE steepness > 0
+GROUP BY steepness,pair
+ORDER BY steepness,pair
+/
+
+-- When steepness < 3 npg lacks CORR() with slope.
+-- But, Negative-CORR() seems noticeable when steepness is 3.
+-- Take a closer look at steepness of 3.
+-- Sign of slope seems like an obvious place to start:
+SELECT
+pair
+,SIGN(ma6_slope)sgn
+,CORR(ma6_slope,npg)
+,AVG(npg)
+,COUNT(npg)
+,MIN(npg)
+,STDDEV(npg)
+,MAX(npg)
+FROM v6hma
+WHERE steepness = 3
+GROUP BY SIGN(ma6_slope),pair
+ORDER BY SIGN(ma6_slope),pair
+/
+
+-- This query shows a subset of the above query but makes it more readable:
+SELECT
+pair
+,ROUND(AVG(npg),4)avg_n_gain
+,ROUND(CORR(ma6_slope,npg),1)Slope_Gain_Correlation
+,COUNT(npg)Rec_count
+FROM v6hma
+WHERE steepness = 3
+AND SIGN(ma6_slope) = 1
+GROUP BY pair
+ORDER BY pair
+/
+
+
 
 exit
--- svm4man/score_j_model.sql
+++ svm4man/score_j_model.sql
@@ -5,7 +5,8 @@
 -- I use this script to join the score table with the model source so I can gauge the effectiveness of SVM.
 
 -- See what I have to work with:
-SELECT COUNT(npg),AVG(npg)FROM svm4ms;
+SELECT MIN(ydate),MAX(ydate), COUNT(npg),AVG(npg)FROM svm4ms;
+
 SELECT COUNT(score),AVG(score)FROM svm4scores;
 
 CREATE OR REPLACE VIEW score_j_model AS
@@ -13,7 +14,10 @@ SELECT pair,ydate,m.prdate,score,npg FROM svm4scores s, svm4ms m
 WHERE s.prdate = m.prdate
 /
 
-SELECT pair,COUNT(score),CORR(score,npg),AVG(npg),SUM(npg)FROM score_j_model GROUP BY pair;
+SELECT 
+pair,COUNT(score),CORR(score,npg),AVG(npg),SUM(npg)
+,MIN(ydate),MAX(ydate)
+FROM score_j_model GROUP BY pair;
 
 SELECT pair,COUNT(score),CORR(score,npg),AVG(npg),SUM(npg),MIN(npg),STDDEV(npg),MAX(npg)
 FROM score_j_model
