2b52d5df85700a2fab98f8f0d3fda83be0650b54
Dan Bikle <bikle@bikle.com>
2010-11-28 08:22:32 +0000

    sparseness

--- 468hma/sparseness.sql
+++ 468hma/sparseness.sql
@@ -17,6 +17,18 @@ ORDER BY ydate,pair
 /
 
 SELECT
+pair
+,SUM(npg4)
+,AVG(npg4)
+FROM v468hma14
+WHERE pair IN('aud_usd','eur_gbp')
+AND sgn4 = -1
+AND ABS(ma4_slope) > 2.1*stddev4
+AND ydate > sysdate -66
+GROUP BY pair
+/
+
+SELECT
 ydate
 ,AVG(npg4)
 ,SUM(npg4)
@@ -29,6 +41,31 @@ AND ABS(ma4_slope) > 2.1*stddev4
 AND ydate > sysdate -66
 GROUP BY ydate
 ORDER BY ydate
+/
+
+SELECT
+pair
+,SUM(npg4)
+,AVG(npg4)
+FROM v468hma14
+WHERE pair IN
+('eur_aud','eur_chf','gbp_aud','gbp_cad','gbp_chf','gbp_jpy','gbp_usd','usd_cad','usd_chf','usd_jpy')
+AND sgn4 = 1
+AND ABS(ma4_slope) > 2.1*stddev4
+AND ydate > sysdate -66
+GROUP BY pair
+ORDER BY pair
+/
+
+SELECT
+SUM(npg4)
+,AVG(npg4)
+FROM v468hma14
+WHERE pair IN
+('eur_aud','eur_chf','gbp_aud','gbp_cad','gbp_chf','gbp_jpy','gbp_usd','usd_cad','usd_chf','usd_jpy')
+AND sgn4 = 1
+AND ABS(ma4_slope) > 2.1*stddev4
+AND ydate > sysdate -66
 /
 
 exit
