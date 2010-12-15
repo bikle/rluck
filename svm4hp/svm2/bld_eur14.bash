f63ad0a168a65380cf5cc97e3fe4db64c5538755
Dan Bikle <bikle@bikle.com>
2010-12-15 01:28:53 +0000

    svm2_scripts

--- svm4hp/svm2/bld_eur14.bash
+++ svm4hp/svm2/bld_eur14.bash
@@ -12,18 +12,18 @@ cd $SVM4HP
 cd svm2/
 
 # eur14t.txt will be the 'top' of eur14.sql
-cat abc14t.txt | sed '1,$s/abc/eur/g' > eur14t.txt
+cat abc14t.txt | sed 's/abc/eur/g' > eur14t.txt
 
 # eur14m.txt will be the 'middle' of eur14.sql
-cat abcxyz.sql | sed '1,$s/abc/eur/g' | sed '1,$s/xyz/eur/g' > eur14m.txt
-cat abcxyz.sql | sed '1,$s/abc/eur/g' | sed '1,$s/xyz/aud/g' >> eur14m.txt
-cat abcxyz.sql | sed '1,$s/abc/eur/g' | sed '1,$s/xyz/gbp/g' >> eur14m.txt
-cat abcxyz.sql | sed '1,$s/abc/eur/g' | sed '1,$s/xyz/jpy/g' >> eur14m.txt
-cat abcxyz.sql | sed '1,$s/abc/eur/g' | sed '1,$s/xyz/cad/g' >> eur14m.txt
-cat abcxyz.sql | sed '1,$s/abc/eur/g' | sed '1,$s/xyz/chf/g' >> eur14m.txt
+cat abcxyz.sql | sed 's/abc/eur/g' | sed 's/xyz/eur/g' > eur14m.txt
+cat abcxyz.sql | sed 's/abc/eur/g' | sed 's/xyz/aud/g' >> eur14m.txt
+cat abcxyz.sql | sed 's/abc/eur/g' | sed 's/xyz/gbp/g' >> eur14m.txt
+cat abcxyz.sql | sed 's/abc/eur/g' | sed 's/xyz/jpy/g' >> eur14m.txt
+cat abcxyz.sql | sed 's/abc/eur/g' | sed 's/xyz/cad/g' >> eur14m.txt
+cat abcxyz.sql | sed 's/abc/eur/g' | sed 's/xyz/chf/g' >> eur14m.txt
 
 # eur14b.txt will be the 'bottom' of eur14.sql
-cat abc14b.txt | sed '1,$s/abc/eur/g' > eur14b.txt
+cat abc14b.txt | sed 's/abc/eur/g' > eur14b.txt
 
 # cat together 
 #   top,        middle,    bottom:
