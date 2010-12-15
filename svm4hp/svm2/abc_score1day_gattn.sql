8da08cb8dd7a46357b6b9317e9d9a3a313ecb91f
Dan Bikle <bikle@bikle.com>
2010-12-15 06:58:28 +0000

    c

--- svm4hp/svm2/abc_score1day_gattn.sql
+++ svm4hp/svm2/abc_score1day_gattn.sql
@@ -229,7 +229,8 @@ WHERE m.ydate = '&1'||' '||'&2'
 
 -- rpt
 -- We should see just 1 row:
-SELECT COUNT(prdate) FROM sme;
+
+SELECT COUNT(prdate) FROM sme
 
 -- Build the model:
 CREATE OR REPLACE VIEW bme AS
@@ -459,8 +460,10 @@ AND 1+m.ydate < '&1'||' '||'&2'
 /
 
 -- rpt
-SELECT gattn, COUNT(prdate) FROM bme GROUP BY gattn;
-SELECT MAX(prdate) FROM bme;
+
+SELECT gattn, COUNT(prdate) FROM bme GROUP BY gattn
+
+SELECT MAX(prdate) FROM bme
 
 -- Now build model from bme and score sme
 @score1gattn.sql
