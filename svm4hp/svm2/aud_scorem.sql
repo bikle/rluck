775d62d703e0ca7b98c91c51009a0369563bdd9e
Dan Bikle <bikle@bikle.com>
2010-12-15 09:23:37 +0000

    enhance_svm4hp

--- svm4hp/svm2/aud_scorem.sql
+++ svm4hp/svm2/aud_scorem.sql
@@ -1,2 +1,2 @@
-@aud_score1day.sql  2010-12-15 08:00:00
-@aud_score1day_gattn.sql  2010-12-15 08:00:00
+@aud_score1day.sql  2010-12-15 09:00:00
+@aud_score1day_gattn.sql  2010-12-15 09:00:00
--- svm4hp/svm2/chf_scorem.sql
+++ svm4hp/svm2/chf_scorem.sql
@@ -1,2 +1,2 @@
-@chf_score1day.sql  2010-12-15 08:00:00
-@chf_score1day_gattn.sql  2010-12-15 08:00:00
+@chf_score1day.sql  2010-12-15 09:00:00
+@chf_score1day_gattn.sql  2010-12-15 09:00:00
