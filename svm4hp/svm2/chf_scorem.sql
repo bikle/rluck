a60cfecbbbc275c10c8e00225ed14417072db304
Dan Bikle <bikle@bikle.com>
2010-12-16 23:13:59 +0000

    unixmacosx.jar.tws

--- svm4hp/svm2/chf_scorem.sql
+++ svm4hp/svm2/chf_scorem.sql
@@ -1,2 +1,2 @@
-@chf_score1day.sql  2010-12-16 22:15:00
-@chf_score1day_gattn.sql  2010-12-16 22:15:00
+@chf_score1day.sql  2010-12-16 22:50:00
+@chf_score1day_gattn.sql  2010-12-16 22:50:00
--- svm4hp/svm2/jpy_scorem.sql
+++
@@ -1,2 +0,0 @@
-@jpy_score1day.sql  2010-12-16 22:20:00
-@jpy_score1day_gattn.sql  2010-12-16 22:20:00
