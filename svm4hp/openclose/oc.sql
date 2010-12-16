53056bd72fc202b367d8a19f6014d80ef6aaafec
Dan Bikle <bikle@bikle.com>
2010-12-16 19:58:09 +0000

    oc.sql

--- svm4hp/openclose/oc.sql
+++ svm4hp/openclose/oc.sql
@@ -12,6 +12,11 @@ SELECT
 prdate
 ,SUBSTR(prdate,1,3)pair
 ,CASE WHEN SUBSTR(prdate,1,3)='aud'THEN'aud_usd'
+      WHEN SUBSTR(prdate,1,3)='eur'THEN'eur_usd'
+      WHEN SUBSTR(prdate,1,3)='gbp'THEN'gbp_usd'
+      WHEN SUBSTR(prdate,1,3)='cad'THEN'usd_cad'
+      WHEN SUBSTR(prdate,1,3)='chf'THEN'usd_chf'
+      WHEN SUBSTR(prdate,1,3)='jpy'THEN'usd_jpy'
  ELSE SUBSTR(prdate,1,3)
  END pair
 ,TO_DATE(SUBSTR(prdate,4,19))ydate
