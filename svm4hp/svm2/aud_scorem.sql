063ba8a64f1e6459717eee9fd4397140c8e679e7
Dan Bikle <bikle@bikle.com>
2010-12-16 05:42:47 +0000

    loop_svm.bash

--- svm4hp/svm2/aud_scorem.sql
+++
@@ -1,2 +0,0 @@
-@aud_score1day.sql  2010-12-16 00:40:00
-@aud_score1day_gattn.sql  2010-12-16 00:40:00
---
+++ svm4hp/svm2/jpy_scorem.sql
@@ -0,0 +1,2 @@
+@jpy_score1day.sql  2010-12-16 03:40:00
+@jpy_score1day_gattn.sql  2010-12-16 03:40:00
