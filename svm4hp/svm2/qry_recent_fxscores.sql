8917bd5857a216ad96fde94bebb3ae5ad4778ca4
Dan Bikle <bikle@bikle.com>
2010-12-17 03:42:43 +0000

    qry_recent_fxscores.sql

--- svm4hp/svm2/qry_recent_fxscores.sql
+++ svm4hp/svm2/qry_recent_fxscores.sql
@@ -2,7 +2,11 @@
 -- qry_recent_fxscores.sql
 --
 
-SELECT * FROM fxscores       WHERE rundate> sysdate -1 ORDER BY rundate;
-SELECT * FROM fxscores_gattn WHERE rundate> sysdate -1 ORDER BY rundate;
+SELECT * FROM fxscores       WHERE rundate> sysdate -1 ORDER BY rundate
+SELECT * FROM fxscores_gattn WHERE rundate> sysdate -1 ORDER BY rundate
+
+SELECT * FROM fxscores       WHERE rundate> sysdate -1 AND score > 0.75 ORDER BY rundate;
+
+SELECT * FROM fxscores_gattn WHERE rundate> sysdate -1 AND score > 0.75 ORDER BY rundate;
 
 exit
