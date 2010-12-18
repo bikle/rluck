3d961b5c8a0954c63c6001fe7e97c29df373bad8
Dan Bikle <bikle@bikle.com>
2010-12-18 19:42:18 +0000

    abc_build_scorem.sql

--- svm8hp/svm2/abc_build_scorem.sql
+++ svm8hp/svm2/abc_build_scorem.sql
@@ -5,7 +5,7 @@
 -- Builds a sql script to run a bunch of sql-scoring scripts.
 
 SELECT '@abc_score1day.sql ',ydate FROM abc_ms14
-WHERE 'abc'||ydate NOT IN (SELECT prdate FROM fxscores)
+WHERE 'abc'||ydate NOT IN (SELECT prdate FROM fxscores8hp)
 -- For backtesting:
 AND ydate > (SELECT MIN(ydate)+35 FROM abc_ms14)
 -- For cron:
@@ -14,7 +14,7 @@ ORDER BY ydate DESC
 /
 
 SELECT '@abc_score1day_gattn.sql ',ydate FROM abc_ms14
-WHERE 'abc'||ydate NOT IN (SELECT prdate FROM fxscores_gattn)
+WHERE 'abc'||ydate NOT IN (SELECT prdate FROM fxscores8hp_gattn)
 -- For backtesting:
 AND ydate > (SELECT MIN(ydate)+35 FROM abc_ms14)
 -- For cron:
