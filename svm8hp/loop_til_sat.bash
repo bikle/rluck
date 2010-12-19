bc7e393e29e103d627e900dfb43267be64a1fdb0
Dan Bikle <bikle@bikle.com>
2010-12-19 22:12:28 +0000

    loop_til_sat.bash

---
+++ svm8hp/loop_til_sat.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# loop_til_sat.bash
+
+# This is a simple loop which keeps running until the calendar hits Saturday.
+
+the_day=`date +"%A"`
+while [ $the_day != 'Saturday' ]
+do
+  echo hello
+  the_day=`date +"%A"`
+  echo Today is $the_day
+  sleep 9
+done
+
+  
