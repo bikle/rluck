f3c1f54090d2fbe03e821833b7acc5385f52bfff
Dan Bikle <bikle@bikle.com>
2010-12-19 23:53:49 +0000

    loop_once.bash

--- svm8hp/loop_once.bash
+++ svm8hp/loop_once.bash
@@ -6,5 +6,41 @@
 
 # It allows me to change what loop_til_sat.bash does without stopping it.
 
-echo $0
-sleep 9
+# I run this script frequently, perhaps every 10 minutes.
+
+. /pt/s/rluck/svm8hp/.orcl
+. /pt/s/rluck/svm8hp/.jruby
+
+set -x 
+
+cd $SVM8HP
+cd ibapi
+export myts=`date +%Y_%m_%d_%H_%M`
+# I need a table:
+./run5min_data.bash > /pt/s/cron/out/run5min_data.${myts}.txt 2>&1
+sqt>/pt/s/cron/out/update_di5min.${myts}.txt<<EOF
+@update_di5min.sql
+EOF
+
+cd ..
+cd svm2/
+
+# Now I generate some scripts from some templates:
+jruby sedem.rb
+
+# Now, for each pair, I create some tables full of model attributes:
+# aud_ms10, aud_att, ... , chf_ms10, chf_att
+./bld_run_big10.bash > /pt/s/cron/out/bld_run_big10.${myts}.txt 2>&1
+# Now, for each pair, run SVM and collect the scores:
+chmod +x *bash
+./aud_svm2.bash > /pt/s/cron/out/aud_svm2.${myts}.txt 2>&1
+# act on the scores:
+cd ..
+cd openclose/
+./oc.bash  > /pt/s/cron/out/oc.bash.${myts}.txt 2>&1
+
+
+# Now do eur_usd:
+
+
+exit
