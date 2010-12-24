2c774d7e6745b68f9f52c463eb55b71b070fb483
Dan Bikle <bikle@bikle.com>
2010-12-24 07:43:51 +0000

    svm6

--- svm8hp/svm2/eur_scorem.sql
+++ svm8hp/svm2/eur_scorem.sql
@@ -1,6 +1,6 @@
-@eur_score1day.sql  2010-12-24 06:25:00
-@eur_score1day.sql  2010-12-24 06:30:00
-@eur_score1day.sql  2010-12-24 06:35:00
-@eur_score1day_gattn.sql  2010-12-24 06:25:00
-@eur_score1day_gattn.sql  2010-12-24 06:30:00
-@eur_score1day_gattn.sql  2010-12-24 06:35:00
+@eur_score1day.sql  2010-12-24 07:25:00
+@eur_score1day.sql  2010-12-24 07:30:00
+@eur_score1day.sql  2010-12-24 07:35:00
+@eur_score1day_gattn.sql  2010-12-24 07:25:00
+@eur_score1day_gattn.sql  2010-12-24 07:30:00
+@eur_score1day_gattn.sql  2010-12-24 07:35:00
