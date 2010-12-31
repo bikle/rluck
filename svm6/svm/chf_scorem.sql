d2bf55ce458bff26a08eba3fbf666db9d9317b18
Dan Bikle <bikle@bikle.com>
2010-12-31 10:17:27 +0000

    avg_recent_scores.sql

--- svm6/svm/chf_scorem.sql
+++ svm6/svm/chf_scorem.sql
@@ -1,6 +1,6 @@
-@chf_score1day.sql  2010-12-31 08:30:00
-@chf_score1day.sql  2010-12-31 08:35:00
-@chf_score1day.sql  2010-12-31 08:40:00
-@chf_score1day_gattn.sql  2010-12-31 08:30:00
-@chf_score1day_gattn.sql  2010-12-31 08:35:00
-@chf_score1day_gattn.sql  2010-12-31 08:40:00
+@chf_score1day.sql  2010-12-31 09:45:00
+@chf_score1day.sql  2010-12-31 09:50:00
+@chf_score1day.sql  2010-12-31 09:55:00
+@chf_score1day_gattn.sql  2010-12-31 09:45:00
+@chf_score1day_gattn.sql  2010-12-31 09:50:00
+@chf_score1day_gattn.sql  2010-12-31 09:55:00
