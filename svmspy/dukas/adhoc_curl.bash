a44f973725192af2a4bae4d406e6db01cfcffaaf
Dan Bikle <bikle@bikle.com>
2011-01-01 21:09:59 +0000

    data4git

--- svmspy/dukas/adhoc_curl.bash
+++ svmspy/dukas/adhoc_curl.bash
@@ -13,7 +13,7 @@ cd /pt/s/rluck/svmspy/dukas/
 export url0='http://www.dukascopy.com/freeApplets/exp/exp.php?'
 
 export url2='fromD='`date -u '+%m.%d.%Y'`
-export url2='fromD=08.11.2010'
+export url2='fromD=10.21.2010'
 
 # 3600 is 1 hr
 export url4='&np=2000&interval=3600&DF=m/d/y'
@@ -27,9 +27,6 @@ export url8='&endSym=unix&split=tz'
 # HTML of form is here:
 # dukasform.html
 
-# dia:
-export url_stock='&Stock=330'
-
 # wmt:
 export url_stock='&Stock=58'
 
@@ -42,6 +39,9 @@ export url_stock='&Stock=329'
 # spy:
 export url_stock='&Stock=331'
 
+# dia:
+export url_stock='&Stock=330'
+
 # Get the data
 export myts=`date -u '+%Y_%m_%d_%H_%M'`
 # echo ${url0}${url2}${url4}${url_EUR_JPY}${url8}
@@ -53,7 +53,7 @@ export socks4a='127.0.0.1:9050'
 
 # curl --socks4a $socks4a --user-agent 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)' --cookie-jar cj.txt --cookie cj.txt --no-buffer --output stock_${myts}.csv ${url0}${url2}${url4}${url_stock}${url8}
 
-curl --user-agent 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)' --cookie-jar cj.txt --cookie cj.txt --no-buffer --output spy_${myts}.10min.csv ${url0}${url2}${url4}${url_stock}${url8}
+curl --user-agent 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)' --cookie-jar cj.txt --cookie cj.txt --no-buffer --output dia_${myts}.10min.csv ${url0}${url2}${url4}${url_stock}${url8}
 
 # Load the data into adhoc table
 
