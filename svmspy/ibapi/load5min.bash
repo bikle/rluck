18f1ecfc319979db01a09b4ecb2eeafb28b946a5
Dan Bikle <bikle@bikle.com>
2011-01-02 20:15:15 +0000

    ibs_stage.ctl

--- svmspy/ibapi/load5min.bash
+++ svmspy/ibapi/load5min.bash
@@ -24,9 +24,22 @@ cd $SVMSPY/ibapi
 
 cd csv_files/
 
-# Loop through the CSV files.
+# Loop through the CSV files belonging to $1:
 
 sort ${1}* |uniq|grep 1|grep -v finished|awk -v awk_var=$1 -F, '{print awk_var","$2","$3}'> ibs_stage.csv
+
+cd ..
+rm -f ibs_stage.csv
+ln -s csv_files/ibs_stage.csv .
+
+sqt<<EOF
+-- @cr_ibs_stage.sql
+EOF
+
+# call sqlloader
+sqlldr trade/t bindsize=20971520 readsize=20971520 rows=123456 control=ibs_stage.ctl
+grep loaded ibs_stage.log
+wc -l ibs_stage.csv
 
 exit 0
 
@@ -38,28 +51,28 @@ fi
 
 exit
 
-sort AUD_USD* |uniq|grep 1|grep -v finished|awk -F, '{print "aud_usd,"$2","$3}'> ibf_stage.csv
-sort EUR_USD* |uniq|grep 1|grep -v finished|awk -F, '{print "eur_usd,"$2","$3}'>> ibf_stage.csv
-sort GBP_USD* |uniq|grep 1|grep -v finished|awk -F, '{print "gbp_usd,"$2","$3}'>> ibf_stage.csv
-sort USD_JPY* |uniq|grep 1|grep -v finished|awk -F, '{print "usd_jpy,"$2","$3}'>> ibf_stage.csv
-sort USD_CAD* |uniq|grep 1|grep -v finished|awk -F, '{print "usd_cad,"$2","$3}'>> ibf_stage.csv
-sort USD_CHF* |uniq|grep 1|grep -v finished|awk -F, '{print "usd_chf,"$2","$3}'>> ibf_stage.csv
+sort AUD_USD* |uniq|grep 1|grep -v finished|awk -F, '{print "aud_usd,"$2","$3}'> ibs_stage.csv
+sort EUR_USD* |uniq|grep 1|grep -v finished|awk -F, '{print "eur_usd,"$2","$3}'>> ibs_stage.csv
+sort GBP_USD* |uniq|grep 1|grep -v finished|awk -F, '{print "gbp_usd,"$2","$3}'>> ibs_stage.csv
+sort USD_JPY* |uniq|grep 1|grep -v finished|awk -F, '{print "usd_jpy,"$2","$3}'>> ibs_stage.csv
+sort USD_CAD* |uniq|grep 1|grep -v finished|awk -F, '{print "usd_cad,"$2","$3}'>> ibs_stage.csv
+sort USD_CHF* |uniq|grep 1|grep -v finished|awk -F, '{print "usd_chf,"$2","$3}'>> ibs_stage.csv
 
 cd ..
-rm -f ibf_stage.csv
-ln -s csv_files/ibf_stage.csv .
+rm -f ibs_stage.csv
+ln -s csv_files/ibs_stage.csv .
 
 sqt<<EOF
--- @cr_ibf_stage.sql
+-- @cr_ibs_stage.sql
 EOF
 
 # call sqlloader
-sqlldr trade/t bindsize=20971520 readsize=20971520 rows=123456 control=ibf_stage.ctl
-grep loaded ibf_stage.log
-wc -l ibf_stage.csv
+sqlldr trade/t bindsize=20971520 readsize=20971520 rows=123456 control=ibs_stage.ctl
+grep loaded ibs_stage.log
+wc -l ibs_stage.csv
 
-sqt>qry_ibf_stage.txt<<EOF
--- @qry_ibf_stage.sql
+sqt>qry_ibs_stage.txt<<EOF
+-- @qry_ibs_stage.sql
 EOF
 
 sqt>merge.txt<<EOF
@@ -67,8 +80,8 @@ sqt>merge.txt<<EOF
 EOF
 
 export myts=`date +%Y_%m_%d_%H_%M`
-cp -p ibf_stage.log ibf_stage_sqlldr.${myts}.log
+cp -p ibs_stage.log ibs_stage_sqlldr.${myts}.log
 cp -p merge.txt merge5min.${myts}.txt
-mv merge5min.${myts}.txt ibf_stage_sqlldr.${myts}.log /pt/s/cron/out/
+mv merge5min.${myts}.txt ibs_stage_sqlldr.${myts}.log /pt/s/cron/out/
 
 exit
