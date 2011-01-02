ade5f2b0a16b22bc43373ab6a1c7104d4e98047a
Dan Bikle <bikle@bikle.com>
2011-01-02 02:09:09 +0000

    data4git

---
+++ svmspy/dukas/data4git/cap_cp.bash
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+cp -p dia_2011_01_01_21_09.10min.csv  DIA_2011_01_01_21_09.10min.csv   
+cp -p dia_2011_01_01_21_10.10min.csv  DIA_2011_01_01_21_10.10min.csv   
+cp -p dia_2011_01_01_21_12.10min.csv  DIA_2011_01_01_21_12.10min.csv   
+cp -p dia_2011_01_01_21_49.10min.csv  DIA_2011_01_01_21_49.10min.csv   
+cp -p dia_2011_01_01_21_52.10min.csv  DIA_2011_01_01_21_52.10min.csv   
+cp -p dia_2011_01_01_22_43.10min.csv  DIA_2011_01_01_22_43.10min.csv   
+cp -p dis_2011_01_01_23_38.10min.csv  DIS_2011_01_01_23_38.10min.csv   
+cp -p dis_2011_01_01_23_39.10min.csv  DIS_2011_01_01_23_39.10min.csv   
+cp -p dis_2011_01_01_23_40.10min.csv  DIS_2011_01_01_23_40.10min.csv   
+cp -p dis_2011_01_01_23_41.10min.csv  DIS_2011_01_01_23_41.10min.csv   
+cp -p dis_2011_01_01_23_42.10min.csv  DIS_2011_01_01_23_42.10min.csv   
+cp -p dis_2011_01_01_23_43.10min.csv  DIS_2011_01_01_23_43.10min.csv   
+cp -p ebay_2011_01_01_23_47.10min.csv EBAY_2011_01_01_23_47.10min.csv  
+cp -p ebay_2011_01_01_23_49.10min.csv EBAY_2011_01_01_23_49.10min.csv  
+cp -p ebay_2011_01_01_23_50.10min.csv EBAY_2011_01_01_23_50.10min.csv  
+cp -p ebay_2011_01_01_23_51.10min.csv EBAY_2011_01_01_23_51.10min.csv  
+cp -p ebay_2011_01_01_23_52.10min.csv EBAY_2011_01_01_23_52.10min.csv  
+cp -p ebay_2011_01_01_23_53.10min.csv EBAY_2011_01_01_23_53.10min.csv  
+cp -p goog_2011_01_01_23_56.10min.csv GOOG_2011_01_01_23_56.10min.csv  
+cp -p goog_2011_01_01_23_58.10min.csv GOOG_2011_01_01_23_58.10min.csv  
+cp -p goog_2011_01_01_23_59.10min.csv GOOG_2011_01_01_23_59.10min.csv  
+cp -p goog_2011_01_02_00_00.10min.csv GOOG_2011_01_02_00_00.10min.csv  
+cp -p goog_2011_01_02_00_01.10min.csv GOOG_2011_01_02_00_01.10min.csv  
+cp -p goog_2011_01_02_00_02.10min.csv GOOG_2011_01_02_00_02.10min.csv  
+cp -p hpq_2011_01_01_23_09.10min.csv  HPQ_2011_01_01_23_09.10min.csv   
+cp -p hpq_2011_01_01_23_11.10min.csv  HPQ_2011_01_01_23_11.10min.csv   
+cp -p hpq_2011_01_01_23_13.10min.csv  HPQ_2011_01_01_23_13.10min.csv   
+cp -p hpq_2011_01_01_23_17.10min.csv  HPQ_2011_01_01_23_17.10min.csv   
+cp -p hpq_2011_01_01_23_18.10min.csv  HPQ_2011_01_01_23_18.10min.csv   
+cp -p hpq_2011_01_01_23_19.10min.csv  HPQ_2011_01_01_23_19.10min.csv   
+cp -p ibm_2011_01_02_00_12.10min.csv  IBM_2011_01_02_00_12.10min.csv   
+cp -p ibm_2011_01_02_00_13.10min.csv  IBM_2011_01_02_00_13.10min.csv   
+cp -p ibm_2011_01_02_00_14.10min.csv  IBM_2011_01_02_00_14.10min.csv   
+cp -p ibm_2011_01_02_00_15.10min.csv  IBM_2011_01_02_00_15.10min.csv   
+cp -p ibm_2011_01_02_00_16.10min.csv  IBM_2011_01_02_00_16.10min.csv   
+cp -p ibm_2011_01_02_00_17.10min.csv  IBM_2011_01_02_00_17.10min.csv   
+cp -p wmt_2011_01_01_22_58.10min.csv  WMT_2011_01_01_22_58.10min.csv   
+cp -p wmt_2011_01_01_22_59.10min.csv  WMT_2011_01_01_22_59.10min.csv   
+cp -p wmt_2011_01_01_23_01.10min.csv  WMT_2011_01_01_23_01.10min.csv   
+cp -p wmt_2011_01_01_23_02.10min.csv  WMT_2011_01_01_23_02.10min.csv   
+cp -p wmt_2011_01_01_23_03.10min.csv  WMT_2011_01_01_23_03.10min.csv   
+cp -p wmt_2011_01_01_23_04.10min.csv  WMT_2011_01_01_23_04.10min.csv   
+cp -p xom_2011_01_01_22_46.10min.csv  XOM_2011_01_01_22_46.10min.csv   
+cp -p xom_2011_01_01_22_50.10min.csv  XOM_2011_01_01_22_50.10min.csv   
+cp -p xom_2011_01_01_22_53.10min.csv  XOM_2011_01_01_22_53.10min.csv   
+cp -p xom_2011_01_01_22_54.10min.csv  XOM_2011_01_01_22_54.10min.csv   
+cp -p xom_2011_01_01_22_55.10min.csv  XOM_2011_01_01_22_55.10min.csv   
+cp -p xom_2011_01_01_22_56.10min.csv  XOM_2011_01_01_22_56.10min.csv   
+
+cp -p X* W* I* H* G* E* D* ../data/
+
+rm  X* W* I* H* G* E* D*
