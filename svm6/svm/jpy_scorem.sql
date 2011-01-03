a83605cdf1f0c2a42ec73706d4be7099d6a05790
Dan Bikle <bikle@bikle.com>
2011-01-03 03:42:00 +0000

    dukas_data4git

--- svm6/svm/jpy_scorem.sql
+++ svm6/svm/jpy_scorem.sql
@@ -1,4 +1,6 @@
-@jpy_score1day.sql  2011-01-03 02:00:00
-@jpy_score1day.sql  2011-01-03 02:05:00
-@jpy_score1day_gattn.sql  2011-01-03 02:00:00
-@jpy_score1day_gattn.sql  2011-01-03 02:05:00
+@jpy_score1day.sql  2011-01-03 03:10:00
+@jpy_score1day.sql  2011-01-03 03:15:00
+@jpy_score1day.sql  2011-01-03 03:20:00
+@jpy_score1day_gattn.sql  2011-01-03 03:10:00
+@jpy_score1day_gattn.sql  2011-01-03 03:15:00
+@jpy_score1day_gattn.sql  2011-01-03 03:20:00
