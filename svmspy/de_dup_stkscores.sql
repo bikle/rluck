ae8054caf42a120b8447caf9cd5409d1c9ca10e5
Dan Bikle <bikle@bikle.com>
2011-01-03 23:09:16 +0000

    svmspy_dedup

--- svmspy/de_dup_stkscores.sql
+++ svmspy/de_dup_stkscores.sql
@@ -4,6 +4,11 @@
 
 -- I use this script after I load stkscores from host-A into host-B.
 
+SET LINES 66
+DESC stktmp
+DESC stkscores
+SET LINES 166
+
 DROP TABLE stktmp;
 PURGE RECYCLEBIN;
 
@@ -22,8 +27,6 @@ GROUP BY tkrdate,targ
 
 SELECT COUNT(*)FROM stkscores;
 SELECT COUNT(*)FROM stktmp;
-
-exit
 
 DROP TABLE stkscores;
 RENAME stktmp TO stkscores;
