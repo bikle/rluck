c368d429b15f1a0e453a97bd475b9963f6518449
Dan Bikle <bikle@bikle.com>
2011-01-03 01:05:56 +0000

    svmhstk

--- svm6/ibapi/order_demos.bash
+++ svm6/ibapi/order_demos.bash
@@ -8,7 +8,7 @@
 #
 #  /pt/s/rluck/svm6/ibapi/place_mkt_ordr_at.bash sell 30000 GBP USD 20110103_04:35:00_GMT
 #
-#  /pt/s/rluck/svm6/ibapi/place_mkt_ordr_at.bash buy 30000 USD CAD 20110103_04:45:00_GMT
+#  /pt/s/rluck/svm6/ibapi/place_mkt_ordr_at.bash buy 30000 USD CAD 20110103_05:00:00_GMT
 
 #  /pt/s/rluck/svm6/ibapi/place_mkt_ordr_at.bash buy 30000 USD CHF 20110103_15:55:30_GMT
 
