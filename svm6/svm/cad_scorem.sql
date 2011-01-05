4bf7f49d07b0eb6c5ad325ef852560e881de36dc
Dan Bikle <bikle@bikle.com>
2011-01-05 15:33:29 +0000

    svmhstk/results_2011_0105_GOOG.txt

--- svm6/svm/cad_scorem.sql
+++ svm6/svm/cad_scorem.sql
@@ -1,6 +1,6 @@
-@cad_score1day.sql  2011-01-05 13:50:00
-@cad_score1day.sql  2011-01-05 13:55:00
-@cad_score1day.sql  2011-01-05 14:00:00
-@cad_score1day_gattn.sql  2011-01-05 13:50:00
-@cad_score1day_gattn.sql  2011-01-05 13:55:00
-@cad_score1day_gattn.sql  2011-01-05 14:00:00
+@cad_score1day.sql  2011-01-05 15:10:00
+@cad_score1day.sql  2011-01-05 15:15:00
+@cad_score1day.sql  2011-01-05 15:20:00
+@cad_score1day_gattn.sql  2011-01-05 15:10:00
+@cad_score1day_gattn.sql  2011-01-05 15:15:00
+@cad_score1day_gattn.sql  2011-01-05 15:20:00
--- svm6/svm/chf_scorem.sql
+++ svm6/svm/chf_scorem.sql
@@ -1,6 +1,6 @@
-@chf_score1day.sql  2011-01-05 13:35:00
-@chf_score1day.sql  2011-01-05 13:40:00
-@chf_score1day.sql  2011-01-05 13:45:00
-@chf_score1day_gattn.sql  2011-01-05 13:35:00
-@chf_score1day_gattn.sql  2011-01-05 13:40:00
-@chf_score1day_gattn.sql  2011-01-05 13:45:00
+@chf_score1day.sql  2011-01-05 14:55:00
+@chf_score1day.sql  2011-01-05 15:00:00
+@chf_score1day.sql  2011-01-05 15:05:00
+@chf_score1day_gattn.sql  2011-01-05 14:55:00
+@chf_score1day_gattn.sql  2011-01-05 15:00:00
+@chf_score1day_gattn.sql  2011-01-05 15:05:00
