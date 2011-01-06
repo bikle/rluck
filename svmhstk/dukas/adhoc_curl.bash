b99785657cd4422256b9f67cd4c60e0dd45f47ad
Dan Bikle <bikle@bikle.com>
2011-01-06 21:12:07 +0000

    data4git

--- svmhstk/dukas/adhoc_curl.bash
+++ svmhstk/dukas/adhoc_curl.bash
@@ -13,7 +13,7 @@ cd /pt/s/rluck/svmhstk/dukas/
 export url0='http://www.dukascopy.com/freeApplets/exp/exp.php?'
 
 export url2='fromD='`date -u '+%m.%d.%Y'`
-export url2='fromD=09.26.2007'
+export url2='fromD=01.26.2011'
 
 # 600 is 10 min
 export url4='&np=2000&interval=600&DF=m/d/y'
