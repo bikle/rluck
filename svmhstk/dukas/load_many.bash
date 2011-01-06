325567db33d2aa84d99503946b6a98babd95a76c
Dan Bikle <bikle@bikle.com>
2011-01-06 21:33:49 +0000

    data4git

--- svmhstk/dukas/load_many.bash
+++ svmhstk/dukas/load_many.bash
@@ -12,17 +12,17 @@ set -x
 cd $SVMHSTK/dukas/
 
 ./load_tkr_1hr.bash DIA
-./load_tkr_1hr.bash GOOG
-./load_tkr_1hr.bash SPY
 
 exit
 
-./load_tkr_1hr.bash QQQQ
 ./load_tkr_1hr.bash DIS 
-./load_tkr_1hr.bash WMT 
-./load_tkr_1hr.bash XOM 
 ./load_tkr_1hr.bash EBAY
+./load_tkr_1hr.bash GOOG
 ./load_tkr_1hr.bash HPQ 
 ./load_tkr_1hr.bash IBM 
+./load_tkr_1hr.bash QQQQ
+./load_tkr_1hr.bash SPY
+./load_tkr_1hr.bash WMT 
+./load_tkr_1hr.bash XOM 
 
 exit 0
