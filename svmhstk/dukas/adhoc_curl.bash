cdfe4fcdcded6a3bb72a76230b82366b85203461
Dan Bikle <bikle@bikle.com>
2011-01-06 21:26:10 +0000

    data4git

--- svmhstk/dukas/adhoc_curl.bash
+++ svmhstk/dukas/adhoc_curl.bash
@@ -14,9 +14,9 @@ export url0='http://www.dukascopy.com/freeApplets/exp/exp.php?'
 
 export url2='fromD='`date -u '+%m.%d.%Y'`
 export url2='fromD=10.29.2008'
-export url2='fromD=01.29.2011'
 export url2='fromD=12.03.2009'
 export url2='fromD=09.26.2007'
+export url2='fromD=01.29.2011'
 
 # 600 is 10 min
 export url4='&np=2000&interval=600&DF=m/d/y'
@@ -30,9 +30,6 @@ export url8='&endSym=unix&split=tz'
 # 331 is SPY.
 # HTML of form is here:
 # dukasform.html
-
-# ebay:
-export url_stock='&Stock=91'
 
 # spy:
 export url_stock='&Stock=331'
@@ -61,6 +58,9 @@ export url_stock='&Stock=44'
 # dis:
 export url_stock='&Stock=38'
 
+# ebay:
+export url_stock='&Stock=91'
+
 # Get the data
 export myts=`date -u '+%Y_%m_%d_%H_%M'`
 # echo ${url0}${url2}${url4}${url_EUR_JPY}${url8}
@@ -72,14 +72,14 @@ export socks4a='127.0.0.1:9050'
 
 # curl --socks4a $socks4a --user-agent 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)' --cookie-jar cj.txt --cookie cj.txt --no-buffer --output stock_${myts}.csv ${url0}${url2}${url4}${url_stock}${url8}
 
-curl --user-agent 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)' --cookie-jar cj.txt --cookie cj.txt --no-buffer --output data4git/DIS_${myts}.1hr.csv ${url0}${url2}${url4}${url_stock}${url8}
+curl --user-agent 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)' --cookie-jar cj.txt --cookie cj.txt --no-buffer --output data4git/EBAY_${myts}.1hr.csv ${url0}${url2}${url4}${url_stock}${url8}
 
 echo sleep 60
 sleep 60
 
 export myts=`date -u '+%Y_%m_%d_%H_%M'`
 
-curl --user-agent 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)' --cookie-jar cj.txt --cookie cj.txt --no-buffer --output data4git/DIS_${myts}.1hr.csv ${url0}${url2}${url4}${url_stock}${url8}
+curl --user-agent 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)' --cookie-jar cj.txt --cookie cj.txt --no-buffer --output data4git/EBAY_${myts}.1hr.csv ${url0}${url2}${url4}${url_stock}${url8}
 
 # Load the data into adhoc table
 
