e39d94d5b6f30753442811ba79fdaa4d507e18f3
Dan Bikle <bikle@bikle.com>
2011-01-06 21:09:04 +0000

    data4git

--- svm6/svm/eur_scorem.sql
+++ svm6/svm/eur_scorem.sql
@@ -1,8 +1,8 @@
-@eur_score1day.sql  2011-01-06 19:20:00
-@eur_score1day.sql  2011-01-06 19:25:00
-@eur_score1day.sql  2011-01-06 19:30:00
-@eur_score1day.sql  2011-01-06 19:35:00
-@eur_score1day_gattn.sql  2011-01-06 19:20:00
-@eur_score1day_gattn.sql  2011-01-06 19:25:00
-@eur_score1day_gattn.sql  2011-01-06 19:30:00
-@eur_score1day_gattn.sql  2011-01-06 19:35:00
+@eur_score1day.sql  2011-01-06 20:45:00
+@eur_score1day.sql  2011-01-06 20:50:00
+@eur_score1day.sql  2011-01-06 20:55:00
+@eur_score1day.sql  2011-01-06 21:00:00
+@eur_score1day_gattn.sql  2011-01-06 20:45:00
+@eur_score1day_gattn.sql  2011-01-06 20:50:00
+@eur_score1day_gattn.sql  2011-01-06 20:55:00
+@eur_score1day_gattn.sql  2011-01-06 21:00:00
