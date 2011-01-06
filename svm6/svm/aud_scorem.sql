e80c014b00e5c1396329afd416c7c956f35299d9
Dan Bikle <bikle@bikle.com>
2011-01-06 20:39:38 +0000

    data4git

--- svm6/svm/aud_scorem.sql
+++ svm6/svm/aud_scorem.sql
@@ -1,8 +1,8 @@
-@aud_score1day.sql  2011-01-06 18:50:00
-@aud_score1day.sql  2011-01-06 18:55:00
-@aud_score1day.sql  2011-01-06 19:00:00
-@aud_score1day.sql  2011-01-06 19:05:00
-@aud_score1day_gattn.sql  2011-01-06 18:50:00
-@aud_score1day_gattn.sql  2011-01-06 18:55:00
-@aud_score1day_gattn.sql  2011-01-06 19:00:00
-@aud_score1day_gattn.sql  2011-01-06 19:05:00
+@aud_score1day.sql  2011-01-06 20:15:00
+@aud_score1day.sql  2011-01-06 20:20:00
+@aud_score1day.sql  2011-01-06 20:25:00
+@aud_score1day.sql  2011-01-06 20:30:00
+@aud_score1day_gattn.sql  2011-01-06 20:15:00
+@aud_score1day_gattn.sql  2011-01-06 20:20:00
+@aud_score1day_gattn.sql  2011-01-06 20:25:00
+@aud_score1day_gattn.sql  2011-01-06 20:30:00
