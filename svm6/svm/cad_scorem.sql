5bd15ecf09bd168aff5f998612f8eb414064f3ad
Dan Bikle <bikle@bikle.com>
2011-01-07 11:21:51 +0000

    cron.txt

--- svm6/svm/cad_scorem.sql
+++ svm6/svm/cad_scorem.sql
@@ -1,6 +1,6 @@
-@cad_score1day.sql  2011-01-07 09:40:00
-@cad_score1day.sql  2011-01-07 09:45:00
-@cad_score1day.sql  2011-01-07 09:50:00
-@cad_score1day_gattn.sql  2011-01-07 09:40:00
-@cad_score1day_gattn.sql  2011-01-07 09:45:00
-@cad_score1day_gattn.sql  2011-01-07 09:50:00
+@cad_score1day.sql  2011-01-07 11:00:00
+@cad_score1day.sql  2011-01-07 11:05:00
+@cad_score1day.sql  2011-01-07 11:10:00
+@cad_score1day_gattn.sql  2011-01-07 11:00:00
+@cad_score1day_gattn.sql  2011-01-07 11:05:00
+@cad_score1day_gattn.sql  2011-01-07 11:10:00
