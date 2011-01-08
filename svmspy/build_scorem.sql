4d11db4c5546e7588f37448ad142579a9f55ca94
Dan Bikle <bikle@bikle.com>
2011-01-08 05:00:26 +0000

    svmtkr.bash

--- svmspy/build_scorem.sql
+++ svmspy/build_scorem.sql
@@ -14,14 +14,14 @@ SELECT '@score1_5min.sql 'cmd,ydate FROM stk_ms
 WHERE ydate NOT IN
   (SELECT ydate FROM stkscores WHERE targ='gatt'AND tkr='&1')
 AND ydate > sysdate - 95
-AND ydate > sysdate - 0.5/24
+-- AND ydate > sysdate - 0.5/24
 AND tkr = '&1'
 UNION
 SELECT '@score1_5min_gattn.sql 'cmd,ydate FROM stk_ms
 WHERE ydate NOT IN
   (SELECT ydate FROM stkscores WHERE targ='gattn'AND tkr='&1')
 AND ydate > sysdate - 95
-AND ydate > sysdate - 0.5/24
+-- AND ydate > sysdate - 0.5/24
 AND tkr = '&1'
 )
 ORDER BY DBMS_RANDOM.VALUE
