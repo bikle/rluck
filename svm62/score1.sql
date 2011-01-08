0cf45598a79b77fb3166fdd9acbeb7966b7fbc3e
Dan Bikle <bikle@bikle.com>
2011-01-08 07:38:13 +0000

    svm62

---
+++ svm62/score1.sql
@@ -0,0 +1,49 @@
+-- score1.sql
+
+-- I use this script to send 5 params to score.sql
+-- which does the heavy lifting of creating an SVM model.
+-- Then at the very end of this script I use the model
+-- inside an INSERT via a call to: PREDICTION_PROBABILITY()
+
+-- I call this script from 2 other scripts:
+-- score1_5min.sql
+-- score1_5min_gattn.sql
+
+-- The 1st param is the name of the target attribute.
+-- I like to call my target attributes either gatt or gattn.
+
+-- Demo:
+-- @score1.sql 'gatt'
+-- @score1.sql 'gattn'
+
+-- Now, I fill up svmc_apply_prep.
+-- I use same model_name used in score.sql
+DEFINE model_name = 'svmfx101'
+DEFINE bldtable   = 'bme'
+DEFINE scoretable = 'sme'
+DEFINE case_id    = 'prdate'
+-- Demo:
+-- @score.sql gatt mymodel   bme         sme           prdate
+@score.sql '&1' '&model_name' '&bldtable' '&scoretable' '&case_id'
+
+-- Maybe I already collected a score for this prdate.
+-- DELETE it if I did:
+DELETE svm62scores
+WHERE score > 0
+AND prdate IN(SELECT prdate FROM svmc_apply_prep)
+-- I need to supply the target attribute name:
+AND targ = '&1'
+/
+
+-- We do a drumroll here:
+
+INSERT INTO svm62scores (prdate,score,rundate,pair,ydate,targ)
+SELECT
+prdate
+,PREDICTION_PROBABILITY(&model_name,'up' USING *)score
+,sysdate
+,SUBSTR(prdate,1,7)pair
+,SUBSTR(prdate,-19)ydate
+,'&1'
+FROM svmc_apply_prep
+/
---
+++ svm62/score1_5min_gattn.sql
@@ -0,0 +1,125 @@
+--
+-- score1_5min_gattn.sql
+--
+
+-- Demo:
+-- @score1_5min_gattn.sql 2010-12-31 20:45:01 aud_usd
+
+CREATE OR REPLACE VIEW sme AS
+SELECT
+prdate
+,NULL gattn
+,g00
+,g01
+,g02
+,g03
+,g04
+,g05
+,g06
+,g07
+,g08
+,g09
+,g10
+,g11
+,g12
+,g13
+,g14
+,g15
+,g16
+,g17
+,g18
+,g19
+,g20
+,g21
+,g22
+,g23
+,g24
+,g25
+,g26
+,g27
+,g28
+,g29
+,g30
+,g31
+,g32
+,g33
+,g34
+,g35
+,g36
+,g37
+,g38
+,g39
+,g40
+,g41
+FROM modsrc
+WHERE ydate = '&1'||' '||'&2'
+AND pair = '&3'
+/
+
+-- rpt
+-- We should see just 1 row:
+
+SELECT COUNT(prdate) FROM sme;
+
+-- Build the model:
+CREATE OR REPLACE VIEW bme AS
+SELECT
+prdate
+,gattn
+,g00
+,g01
+,g02
+,g03
+,g04
+,g05
+,g06
+,g07
+,g08
+,g09
+,g10
+,g11
+,g12
+,g13
+,g14
+,g15
+,g16
+,g17
+,g18
+,g19
+,g20
+,g21
+,g22
+,g23
+,g24
+,g25
+,g26
+,g27
+,g28
+,g29
+,g30
+,g31
+,g32
+,g33
+,g34
+,g35
+,g36
+,g37
+,g38
+,g39
+,g40
+,g41
+FROM modsrc
+WHERE gattn IN('nup','up')
+-- Use only rows which are older than 1 day:
+AND 1+ydate < '&1'||' '||'&2'
+AND pair = '&3'
+/
+
+-- rpt
+
+SELECT gattn, COUNT(prdate) FROM bme GROUP BY gattn;
+
+SELECT MAX(prdate) FROM bme;
+
+-- Now build model from bme and score sme
+@score1.sql gattn
