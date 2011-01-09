72323fec6d49d3e9b2f520e8e46ee3539af75965
Dan Bikle <bikle@bikle.com>
2011-01-09 23:00:03 +0000

    svm62

--- svm62/openclose/avg_recent_scores.sql
+++ svm62/openclose/avg_recent_scores.sql
@@ -9,32 +9,35 @@
 -- done already
 CREATE OR REPLACE VIEW ocj AS
 SELECT
-s.prdate
-,s.score
-,s.rundate
-,s.pair
-,s.ydate
-,g.score gscore
+l.prdate
+,l.score score_long
+,s.score score_short
+,l.rundate
+,l.pair
+,l.ydate
 ,p.clse
-FROM fxscores6 s
-,fxscores6_gattn g
+FROM svm62scores l
+,svm62scores s
 ,di5min p
-WHERE s.prdate = g.prdate
-AND REPLACE(REPLACE(p.pair,'usd_',''),'_usd','') = s.pair
-AND s.ydate = p.ydate
+WHERE l.prdate = s.prdate
+AND p.pair = l.pair
+AND l.ydate = p.ydate
+AND l.targ = 'gatt'
+AND s.targ = 'gattn'
 /
 
 -- done already
 
 -- 1st I show the actual scores:
-@qry_recent_fxscores.sql
+-- @qry_recent_fxscores.sql
 
 -- Look for good longs:
+
 CREATE OR REPLACE VIEW good_longs AS
 SELECT
 pair
-,AVG(score) long_score
-,AVG(gscore)short_score
+,AVG(score_long) avg_score_long
+,AVG(score_short) avg_score_short
 ,MIN(ydate) mndate
 ,COUNT(pair)cnt
 ,MAX(ydate) mxdate
@@ -44,16 +47,17 @@ pair
 FROM ocj
 WHERE ydate > sysdate - 2/24
 GROUP BY pair
-HAVING(AVG(score) > 0.6 AND AVG(gscore) < 0.4)
+HAVING(AVG(score_long) > 0.6 AND AVG(score_short) < 0.4)
 ORDER BY MAX(rundate)
 /
 
 -- Look for good shorts:
+
 CREATE OR REPLACE VIEW good_shorts AS
 SELECT
 pair
-,AVG(score) long_score
-,AVG(gscore)short_score
+,AVG(score_long) avg_score_long
+,AVG(score_short) avg_score_short
 ,MIN(ydate) mndate
 ,COUNT(pair)cnt
 ,MAX(ydate) mxdate
@@ -63,9 +67,10 @@ pair
 FROM ocj
 WHERE ydate > sysdate - 2/24
 GROUP BY pair
-HAVING(AVG(gscore) > 0.6 AND AVG(score) < 0.4)
+HAVING(AVG(score_short) > 0.6 AND AVG(score_long) < 0.4)
 ORDER BY MAX(rundate)
 /
+
 
 SELECT * FROM good_longs;
 
@@ -76,8 +81,8 @@ SELECT * FROM good_shorts;
 CREATE OR REPLACE VIEW glp AS
 SELECT
 g.pair
-,g.long_score
-,g.short_score
+,g.avg_score_long
+,g.avg_score_short
 ,g.mndate
 ,g.cnt
 ,g.mxdate
@@ -93,8 +98,8 @@ WHERE g.pair = p.pair
 CREATE OR REPLACE VIEW gsp AS
 SELECT
 g.pair
-,g.long_score
-,g.short_score
+,g.avg_score_long
+,g.avg_score_short
 ,g.mndate
 ,g.cnt
 ,g.mxdate
