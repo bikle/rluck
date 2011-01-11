c6d523bb5e7a66182c6bb9f387704eca31f2fbba
Dan Bikle <bikle@bikle.com>
2011-01-11 05:34:49 +0000

    good_svmtkr.bash

---
+++ svmd/good_svmtkr.bash
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# many_svmtkr.bash
+
+cd /pt/s/rluck/svmd/
+
+set -x
+
+./svmtkr.bash XLB     
+./svmtkr.bash V       
+./svmtkr.bash VMW     
+./svmtkr.bash TLT     
+./svmtkr.bash SLV     
+./svmtkr.bash BRCM    
+./svmtkr.bash DIA     
+./svmtkr.bash AMT     
+./svmtkr.bash VECO    
+./svmtkr.bash ALL     
+./svmtkr.bash WDC     
+./svmtkr.bash DD      
+./svmtkr.bash CREE    
+./svmtkr.bash AKAM    
+./svmtkr.bash COF     
+./svmtkr.bash WMT     
+./svmtkr.bash AMZN    
+./svmtkr.bash CRM     
+./svmtkr.bash XLU     
+./svmtkr.bash XLE     
+./svmtkr.bash DIS     
+./svmtkr.bash ABX     
+./svmtkr.bash BIDU    
