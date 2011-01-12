f1f0d83ea76663eb4bf44b46719970c65760ab4a
Dan Bikle <bikle@bikle.com>
2011-01-12 14:56:43 +0000

    avg_recent_good.sql

---
+++ svmd/avg_recent_good.sql
@@ -0,0 +1,60 @@
+--
+-- avg_recent_good.sql
+--
+
+
+SELECT
+l.tkr
+,AVG(l.score)score_long
+,AVG(s.score)score_short
+,COUNT(l.tkr)ccount
+,MAX(l.ydate)mx_ydate
+FROM ystkscores l, ystkscores s, good_tkrs_svmd_v g
+WHERE s.tkrdate = l.tkrdate
+AND l.tkr = g.tkr
+AND s.targ = 'gattn'
+AND l.targ = 'gatt'
+AND l.ydate > sysdate - 2
+GROUP BY l.tkr
+ORDER BY l.tkr
+/
+
+
+
+SELECT
+l.tkr
+,AVG(l.score)score_long
+,AVG(s.score)score_short
+,COUNT(l.tkr)ccount
+,MAX(l.ydate)mx_ydate
+FROM ystkscores l, ystkscores s, good_tkrs_svmd_v g
+WHERE s.tkrdate = l.tkrdate
+AND l.tkr = g.tkr
+AND s.targ = 'gattn'
+AND l.targ = 'gatt'
+AND l.ydate > sysdate - 2
+GROUP BY l.tkr
+HAVING AVG(l.score) > 0.7
+ORDER BY l.tkr
+/
+
+SELECT
+l.tkr
+,AVG(l.score)score_long
+,AVG(s.score)score_short
+,COUNT(l.tkr)ccount
+,MAX(l.ydate)mx_ydate
+FROM ystkscores l, ystkscores s, good_tkrs_svmd_v g
+WHERE s.tkrdate = l.tkrdate
+AND l.tkr = g.tkr
+AND s.targ = 'gattn'
+AND l.targ = 'gatt'
+AND l.ydate > sysdate - 2
+GROUP BY l.tkr
+HAVING AVG(S.score) > 0.7
+ORDER BY l.tkr
+/
+
+
+
+exit
