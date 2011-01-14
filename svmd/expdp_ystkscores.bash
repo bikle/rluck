fe58e2b593e67fdfa40eed9859a07785b260babb
Dan Bikle <bikle@bikle.com>
2011-01-14 11:10:10 +0000

    expdp_di5.bash

--- svmd/expdp_ystkscores.bash
+++ svmd/expdp_ystkscores.bash
@@ -13,5 +13,6 @@ expdp trade/t dumpfile=ystkscores.${myts}.dpdmp tables=ystkscores
 echo "scp -p /oracle/app/oracle/admin/orcl/dpdump/ystkscores.${myts}.dpdmp z:/oracle/app/oracle/admin/orcl/dpdump/"
 echo "scp -p /oracle/app/oracle/admin/orcl/dpdump/ystkscores.${myts}.dpdmp h:/oracle/app/oracle/admin/orcl/dpdump/"
 echo "scp -p /oracle/app/oracle/admin/orcl/dpdump/ystkscores.${myts}.dpdmp z2:/oracle/app/oracle/admin/orcl/dpdump/"
+echo "scp -p /oracle/app/oracle/admin/orcl/dpdump/ystkscores.${myts}.dpdmp z3:/oracle/app/oracle/admin/orcl/dpdump/"
 
 echo "impdp trade/t table_exists_action=append dumpfile=ystkscores.${myts}.dpdmp"
