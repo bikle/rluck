d97fa3b943c6f2d725edc8c5b21558f24c15699c
Dan Bikle <bikle@bikle.com>
2011-01-14 23:01:10 +0000

    login.sql

--- svmspy/ibapi/j.sql
+++ svmspy/ibapi/j.sql
@@ -1,38 +1,10 @@
 
-DROP TABLE   di5min_stk_fd;
-CREATE TABLE di5min_stk_fd COMPRESS AS
-SELECT
-tkrdate
-,tkr
-,ydate
-,clse
-,CASE WHEN TO_CHAR(ydate,'dy')IN('mon','tue','wed','thu')THEN ydate+1
-      WHEN TO_CHAR(ydate,'dy')IN('fri')THEN ydate+3
-      ELSE NULL END selldate
-FROM di5min_stk
-/
-
--- Join with the future
-
-DROP   TABLE di5min_stk_c2;
-
-CREATE TABLE di5min_stk_c2 COMPRESS AS
-SELECT
-p.tkrdate
-,p.tkr
-,p.ydate
-,p.clse
-,p.selldate
-,f.clse clse2
-FROM di5min_stk_fd p, di5min_stk f
-WHERE p.tkr || p.selldate = f.tkrdate(+)
-/
-
 -- rpt
-SELECT TO_CHAR(ydate,'dy')dday,COUNT(tkr) FROM di5min_stk_fd group by TO_CHAR(ydate,'dy');
-SELECT TO_CHAR(ydate,'dy')dday,COUNT(tkr) FROM di5min_stk_c2 group by TO_CHAR(ydate,'dy');
-
-SELECT MAX(ydate)FROM di5min_stk;
-SELECT MAX(ydate)FROM di5min_stk_c2;
-
-PURGE RECYCLEBIN;
+SELECT
+TO_CHAR(ydate,'Day')
+,COUNT(tkr)
+FROM dukas5min_stk
+WHERE ydate < '2010-12-27 09:00:00'
+GROUP BY TO_CHAR(ydate,'Day')
+ORDER BY TO_CHAR(ydate,'Day')
+/
