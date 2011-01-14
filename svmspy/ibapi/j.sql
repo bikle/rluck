089b06ecf7bcdfcac760541db7c3c3733f35e7aa
Dan Bikle <bikle@bikle.com>
2011-01-14 06:27:25 +0000

    update_di5min_stk.sql

--- svmspy/ibapi/j.sql
+++ svmspy/ibapi/j.sql
@@ -1,27 +1,38 @@
-CREATE TABLE dropme COMPRESS AS
+
+DROP TABLE   di5min_stk_fd;
+CREATE TABLE di5min_stk_fd COMPRESS AS
 SELECT
-prdate
-,pair
+tkrdate
+,tkr
 ,ydate
 ,clse
-FROM
-(
-  SELECT
-  prdate
-  ,pair
-  ,ydate
-  ,AVG(clse)clse
-  FROM di5min0
-  GROUP BY 
-  prdate
-  ,pair
-  ,ydate
-  UNION
-  SELECT
-  prdate
-  ,pair
-  ,ydate
-  ,clse
-  FROM op5min
-)
+,CASE WHEN TO_CHAR(ydate,'dy')IN('mon','tue','wed','thu')THEN ydate+1
+      WHEN TO_CHAR(ydate,'dy')IN('fri')THEN ydate+3
+      ELSE NULL END selldate
+FROM di5min_stk
 /
+
+-- Join with the future
+
+DROP   TABLE di5min_stk_c2;
+
+CREATE TABLE di5min_stk_c2 COMPRESS AS
+SELECT
+p.tkrdate
+,p.tkr
+,p.ydate
+,p.clse
+,p.selldate
+,f.clse clse2
+FROM di5min_stk_fd p, di5min_stk f
+WHERE p.tkr || p.selldate = f.tkrdate(+)
+/
+
+-- rpt
+SELECT TO_CHAR(ydate,'dy')dday,COUNT(tkr) FROM di5min_stk_fd group by TO_CHAR(ydate,'dy');
+SELECT TO_CHAR(ydate,'dy')dday,COUNT(tkr) FROM di5min_stk_c2 group by TO_CHAR(ydate,'dy');
+
+SELECT MAX(ydate)FROM di5min_stk;
+SELECT MAX(ydate)FROM di5min_stk_c2;
+
+PURGE RECYCLEBIN;
--- svmspy/ibapi/update_di5min_stk.sql
+++ svmspy/ibapi/update_di5min_stk.sql
@@ -74,6 +74,8 @@ tkrdate
 ,ydate
 ,AVG(clse)clse
 FROM di5min_stk0
+-- I want weekdays:
+WHERE 0+TO_CHAR(ydate,'D')IN(2,3,4,5,6)
 GROUP BY 
 tkrdate
 ,tkr
@@ -92,6 +94,45 @@ SELECT COUNT(*)FROM di5min_stk WHERE TO_CHAR(ydate,'SS')='01';
 
 UPDATE di5min_stk SET tkrdate = tkr||ydate;
 
+-- Add a column to help join with future dates
+-- and then calculate 1 day gain:
+DROP TABLE   di5min_stk_fd;
+CREATE TABLE di5min_stk_fd COMPRESS AS
+SELECT
+tkrdate
+,tkr
+,ydate
+,clse
+,CASE WHEN TO_CHAR(ydate,'dy')IN('mon','tue','wed','thu')THEN ydate+1
+      WHEN TO_CHAR(ydate,'dy')IN('fri')THEN ydate+3
+      ELSE NULL END selldate
+FROM di5min_stk
+/
+
+-- Join with the future
+
+DROP   TABLE di5min_stk_c2;
+
+CREATE TABLE di5min_stk_c2 COMPRESS AS
+SELECT
+p.tkrdate
+,p.tkr
+,p.ydate
+,p.clse
+,p.selldate
+,f.clse clse2
+,f.clse - p.clse gain1day
+FROM di5min_stk_fd p, di5min_stk f
+-- Use a left-outer-join so I get recent rows in di5min_stk_fd:
+WHERE p.tkr || p.selldate = f.tkrdate(+)
+/
+
+-- rpt
+SELECT TO_CHAR(ydate,'dy')dday,COUNT(tkr) FROM di5min_stk_fd group by TO_CHAR(ydate,'dy');
+SELECT TO_CHAR(ydate,'dy')dday,COUNT(tkr) FROM di5min_stk_c2 group by TO_CHAR(ydate,'dy');
+SELECT MAX(ydate)FROM di5min_stk;
+SELECT MAX(ydate)FROM di5min_stk_c2;
+
 -- rpt
 SELECT
 tkr
@@ -101,7 +142,8 @@ tkr
 ,MIN(clse)
 ,AVG(clse)
 ,MAX(clse)
-FROM di5min_stk
+,MAX(gain1day)
+FROM di5min_stk_c2
 GROUP BY tkr
 ORDER BY tkr
 /
@@ -110,8 +152,10 @@ COLUMN clse FORMAT 999.9999
 SELECT
 tkr
 ,ROUND(clse,4)clse
+-- Should be NULL:
+,ROUND(clse2,4)clse2
 ,ydate
-FROM di5min_stk
+FROM di5min_stk_c2
 WHERE ydate > sysdate - 0.5/24
 ORDER BY tkr,ydate
 /
