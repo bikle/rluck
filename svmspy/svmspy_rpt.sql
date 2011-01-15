4c14e629704d30f5b89c41a2480e6522238fd064
Dan Bikle <bikle@bikle.com>
2011-01-15 07:55:46 +0000

    svmspy_rpt.sql

--- svmspy/svmspy_rpt.sql
+++ svmspy/svmspy_rpt.sql
@@ -60,6 +60,86 @@ GROUP BY tkr,rscore_short
 ORDER BY tkr,rscore_short
 /
 
+-- This works better for sparse results:
+
+CREATE OR REPLACE VIEW sspy10l AS
+SELECT
+l.tkr
+,l.score score_long
+,gain1day g1d
+,l.ydate
+,ROUND(l.score,1)rscore_long
+FROM stkscores l, di5min_stk_c2 g
+WHERE l.targ='gatt'
+AND l.tkrdate=g.tkrdate
+/
+
+SELECT
+tkr
+,ccount
+,crr_l
+FROM
+(
+  SELECT
+  tkr
+  ,CORR(score_long,g1d)crr_l
+  ,MIN(ydate)mn_date
+  ,COUNT(tkr)ccount
+  ,MAX(ydate)mx_date
+  FROM sspy10l
+  GROUP BY tkr
+)
+/
+
+SELECT
+tkr
+,rscore_long
+,AVG(g1d)
+,COUNT(g1d)
+FROM sspy10l
+GROUP BY tkr,rscore_long
+ORDER BY tkr,rscore_long
+/
+
+
+CREATE OR REPLACE VIEW sspy10s AS
+SELECT
+l.tkr
+,l.score score_short
+,gain1day g1d
+,l.ydate
+,ROUND(l.score,1)rscore_short
+FROM stkscores l, di5min_stk_c2 g
+WHERE l.targ='gattn'
+AND l.tkrdate=g.tkrdate
+/
+
+SELECT
+tkr
+,ccount
+,crr_l
+FROM
+(
+  SELECT
+  tkr
+  ,CORR(score_short,g1d)crr_l
+  ,MIN(ydate)mn_date
+  ,COUNT(tkr)ccount
+  ,MAX(ydate)mx_date
+  FROM sspy10s
+  GROUP BY tkr
+)
+/
+
+SELECT
+tkr
+,rscore_short
+,AVG(g1d)
+,COUNT(g1d)
+FROM sspy10s
+GROUP BY tkr,rscore_short
+ORDER BY tkr,rscore_short
+/
 
 exit
 
