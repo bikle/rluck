c2b86a0bc10c8bbb41c17b9bed318d8d680ff7b6
Dan Bikle <bikle@bikle.com>
2011-01-16 13:03:09 +0000

    svmspy

--- svmspy/build_scorem.sql
+++ svmspy/build_scorem.sql
@@ -10,14 +10,14 @@
 
 SELECT cmd,ydate,'&1' tkr FROM
 (
-SELECT '@score1_5min.sql 'cmd,ydate FROM stk_ms
+SELECT '@score1_5min.sql 'cmd,ydate FROM stk_ms_svmspy
 WHERE ydate NOT IN
   (SELECT ydate FROM stkscores WHERE targ='gatt'AND tkr='&1')
 AND ydate > '2010-12-01'
 -- AND ydate > sysdate - 0.5/24
 AND tkr = '&1'
 UNION
-SELECT '@score1_5min_gattn.sql 'cmd,ydate FROM stk_ms
+SELECT '@score1_5min_gattn.sql 'cmd,ydate FROM stk_ms_svmspy
 WHERE ydate NOT IN
   (SELECT ydate FROM stkscores WHERE targ='gattn'AND tkr='&1')
 AND ydate > '2010-12-01'
