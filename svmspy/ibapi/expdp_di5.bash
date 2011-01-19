8bbb7e53eab75da7d1a6d00fb6f86146f8cc2c01
Dan Bikle <bikle@bikle.com>
2011-01-19 23:32:30 +0000

    expdp_di5.bash

--- svmspy/ibapi/expdp_di5.bash
+++ svmspy/ibapi/expdp_di5.bash
@@ -11,13 +11,13 @@
 # ibs_old
 # ibs_dups_old
 # ibs_stage
-
+# I pickup stkscores too.
 . /pt/s/rluck/svmspy/.orcl
 
 set -x
 export myts=`date +%Y_%m_%d_%H_%M`
 
-expdp trade/t dumpfile=svmspy_di5.${myts}.dpdmp tables=di5min_stk0,di5min_stk,ibs5min,dukas5min_stk,dukas10min_stk,ibs_old,ibs_dups_old,ibs_stage,di5min_stk_c2
+expdp trade/t dumpfile=svmspy_di5.${myts}.dpdmp tables=di5min_stk0,di5min_stk,ibs5min,dukas5min_stk,dukas10min_stk,ibs_old,ibs_dups_old,ibs_stage,di5min_stk_c2,stkscores
 
 echo "scp -p /oracle/app/oracle/admin/orcl/dpdump/svmspy_di5.${myts}.dpdmp usr10@xp:dpdump/"
 echo "scp -p /oracle/app/oracle/admin/orcl/dpdump/svmspy_di5.${myts}.dpdmp l:/oracle/app/oracle/admin/orcl/dpdump/"
@@ -30,6 +30,7 @@ echo "impdp trade/t table_exists_action=append dumpfile=svmspy_di5.${myts}.dpdmp
 echo OR
 echo "impdp trade/t table_exists_action=replace dumpfile=svmspy_di5.${myts}.dpdmp"
 echo "impdp trade/t table_exists_action=replace dumpfile=svmspy_di5.${myts}.dpdmp tables=di5min_stk_c2"
+echo "impdp trade/t table_exists_action=append dumpfile=svmspy_di5.${myts}.dpdmp tables=stkscores"
 
 exit
 
