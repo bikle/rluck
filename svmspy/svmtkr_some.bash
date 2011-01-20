ebf7913891d79092b7b2d75527e07f137bdde0d2
Dan Bikle <bikle@bikle.com>
2011-01-20 19:06:08 +0000

    svmtkr_some.bash

---
+++ svmspy/svmtkr_some.bash
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# loop_some.bash
+
+. /pt/s/rluck/svmspy/.orcl
+. /pt/s/rluck/svmspy/.jruby
+
+set -x
+
+date
+
+cd $SVMSPY
+
+## ./svmtkr.bash MJN
+## ./svmtkr.bash LFT
+## ./svmtkr.bash V
+## ./svmtkr.bash XLB
+## ./svmtkr.bash VMW
+## ./svmtkr.bash TLT
+## ./svmtkr.bash QQQQ
+## ./svmtkr.bash PM
+## ./svmtkr.bash IOC
+## ./svmtkr.bash SJM
+## ./svmtkr.bash NEM
+## ./svmtkr.bash GDX
+## ./svmtkr.bash AMT
+## ./svmtkr.bash DIA
+## ./svmtkr.bash SKX
+## ./svmtkr.bash BRCM
+## ./svmtkr.bash GLD
+## ./svmtkr.bash VECO
+## ./svmtkr.bash ALL
+## ./svmtkr.bash WDC
+## ./svmtkr.bash KO
+
+# Run this on z3 after import of svmspy_di5.${myts}.dpdmp:
+
+./svmtkr.bash SLV
+./svmtkr.bash AKAM
+./svmtkr.bash QCOM
+./svmtkr.bash PEP
+./svmtkr.bash GOOG
+./svmtkr.bash IBM
+./svmtkr.bash DD
+./svmtkr.bash CREE
+./svmtkr.bash MRK
+./svmtkr.bash SNDK
+./svmtkr.bash COF
+./svmtkr.bash WMT
+./svmtkr.bash WYNN
+./svmtkr.bash IWM
+./svmtkr.bash AMZN
+./svmtkr.bash MCD
+./svmtkr.bash GS
+./svmtkr.bash SPY
+./svmtkr.bash EFA
+./svmtkr.bash CRM
+./svmtkr.bash XLE
+./svmtkr.bash XLU
+./svmtkr.bash OIH
+./svmtkr.bash DIS
+./svmtkr.bash ABX
+./svmtkr.bash PG
+./svmtkr.bash MAR
+./svmtkr.bash FSLR
+./svmtkr.bash BIDU
+./svmtkr.bash MET
