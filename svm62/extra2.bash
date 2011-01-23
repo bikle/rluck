137c3481af509d39c2ded9a79d6eb10d5a3d537b
Dan Bikle <bikle@bikle.com>
2011-01-23 23:29:33 +0000

    extra.bash

---
+++ svm62/extra2.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# extra2.bash
+
+# I use this script to add extra functionality to other scripts.
+
+# Now this script is empty.
+
+echo $0 called
+sleep 1
+echo $0 finished
+
+exit 0
