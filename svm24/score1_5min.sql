5d3f23a1c380c6cb3c2dbd3595a0f1b4aa8a5d1f
Dan Bikle <bikle@bikle.com>
2011-01-30 03:07:22 +0000

    svm24/score1_5min.sql

---
+++ svm24/score1_5min.sql
@@ -0,0 +1,130 @@
+--
+-- score1_5min.sql
+--
+
+-- Demo:
+-- @score1_5min.sql 2010-12-31 20:45:01 aud_usd
+
+CREATE OR REPLACE VIEW sme AS
+SELECT
+prdate
+,NULL gatt
+,g00
+,g01
+,g02
+,g03
+,g04
+,g05
+,g06
+,g07
+,g08
+,g09
+,g10
+,g11
+,g12
+,g13
+,g14
+,g15
+,g16
+,g17
+,g18
+,g19
+,g20
+,g21
+,g22
+,g23
+,g24
+,g25
+,g26
+,g27
+,g28
+,g29
+,g30
+,g31
+,g32
+,g33
+,g34
+,g35
+,g36
+,g37
+,g38
+,g39
+,g40
+,g41
+,s.sc_corr
+FROM modsrc24
+,(SELECT AVG(sc_corr)sc_corr FROM modsrc24 WHERE gatt='up'AND 11+ydate>(SELECT MAX(ydate)FROM modsrc24))s
+WHERE ydate = '&1'||' '||'&2'
+AND pair = '&3'
+/
+
+-- rpt
+-- We should see just 1 row:
+
+SELECT COUNT(prdate) FROM sme;
+
+SELECT AVG(sc_corr)sc_corr FROM modsrc24 WHERE gatt='up'AND 11+ydate>(SELECT MAX(ydate)FROM modsrc24);
+
+-- Build the model:
+CREATE OR REPLACE VIEW bme AS
+SELECT
+prdate
+,gatt
+,g00
+,g01
+,g02
+,g03
+,g04
+,g05
+,g06
+,g07
+,g08
+,g09
+,g10
+,g11
+,g12
+,g13
+,g14
+,g15
+,g16
+,g17
+,g18
+,g19
+,g20
+,g21
+,g22
+,g23
+,g24
+,g25
+,g26
+,g27
+,g28
+,g29
+,g30
+,g31
+,g32
+,g33
+,g34
+,g35
+,g36
+,g37
+,g38
+,g39
+,g40
+,g41
+,sc_corr
+FROM modsrc24
+WHERE gatt IN('nup','up')
+-- Use only rows which are older than 1 day:
+AND 1+ydate < '&1'||' '||'&2'
+AND pair = '&3'
+/
+
+-- rpt
+
+SELECT gatt, COUNT(prdate) FROM bme GROUP BY gatt;
+
+SELECT MAX(prdate) FROM bme;
+
+-- Now build model from bme and score sme
+@score1.sql gatt
