de2243aeb933fe7e4bc3e4cc6a48f0e9e6a66071
Dan Bikle <bikle@bikle.com>
2011-02-20 15:26:12 +0000

    btg2.sql

--- svm62/btg2.sql
+++ svm62/btg2.sql
@@ -47,6 +47,7 @@ m.pair
 ,ROUND(l.score-s.score,2) rscore_diff2
 ,m.g2
 ,m.g6
+,m.g6-m.g2 g4
 FROM svm62scores l,svm62scores s,btg10 m
 WHERE l.targ='gatt'
 AND   s.targ='gattn'
@@ -61,96 +62,80 @@ AND s.ydate > sysdate - 133
 SELECT
 pair
 ,rscore_diff1
+,CORR(score_diff,g6)
 ,AVG(g2)
 ,AVG(g6)
+,AVG(g4)
 ,CORR(g2,g6)
+,CORR(g2,g4)
 ,MIN(ydate)
 ,COUNT(ydate)
 ,MAX(ydate)
 FROM btg12
-WHERE rscore_diff1 > 0.7
+WHERE ABS(rscore_diff1)IN(0.7,0.8)
 GROUP BY pair,rscore_diff1
 ORDER BY pair,rscore_diff1
 /
 
--- rpt
-
-exit
-
-CREATE OR REPLACE VIEW scc14 AS
 SELECT
-pair
-,ydate
-,prdate
-,score_diff
-,g6
-,COVAR_POP(score_diff,g6)OVER(PARTITION BY pair ORDER BY ydate ROWS BETWEEN 12*24*1 PRECEDING AND CURRENT ROW)cpp1
-,COVAR_POP(score_diff,g6)OVER(PARTITION BY pair ORDER BY ydate ROWS BETWEEN CURRENT ROW AND 12*24*1 FOLLOWING)cpf1
-,COVAR_POP(score_diff,g6)OVER(PARTITION BY pair ORDER BY ydate ROWS BETWEEN 12*24*2 PRECEDING AND CURRENT ROW)cpp2
-,COVAR_POP(score_diff,g6)OVER(PARTITION BY pair ORDER BY ydate ROWS BETWEEN CURRENT ROW AND 12*24*2 FOLLOWING)cpf2
-,COVAR_POP(score_diff,g6)OVER(PARTITION BY pair ORDER BY ydate ROWS BETWEEN 12*24*3 PRECEDING AND CURRENT ROW)cpp3
-,COVAR_POP(score_diff,g6)OVER(PARTITION BY pair ORDER BY ydate ROWS BETWEEN CURRENT ROW AND 12*24*3 FOLLOWING)cpf3
+SIGN(g2) * SIGN(score_diff) sprod
+,rscore_diff1
+,CORR(score_diff,g6)
+,AVG(g2)
+,AVG(g6)
+,AVG(g4)
+,CORR(g2,g6)
+,CORR(g2,g4)
+,MIN(ydate)
+,COUNT(ydate)
+,MAX(ydate)
 FROM btg12
+WHERE ABS(rscore_diff1)IN(0.7,0.8)
+AND SIGN(g2) != 0
+AND ABS(g2) < 0.0006
+GROUP BY rscore_diff1,(SIGN(g2) * SIGN(score_diff))
+ORDER BY rscore_diff1,(SIGN(g2) * SIGN(score_diff))
 /
 
-DROP TABLE scc16_t;
-CREATE TABLE scc16_t COMPRESS AS
 SELECT
-pair
-,ydate
-,score_diff
-,g6
-,cpp1
-,cpp2
-,cpp3
-,CORR(cpp1,cpf1)OVER(PARTITION BY pair ORDER BY ydate)crr1
-,CORR(cpp2,cpf2)OVER(PARTITION BY pair ORDER BY ydate)crr2
-,CORR(cpp3,cpf3)OVER(PARTITION BY pair ORDER BY ydate)crr3
-FROM scc14
+SIGN(g2) * SIGN(score_diff) sprod
+,rscore_diff1
+,AVG(g4)avg_g4
+,CORR(score_diff,g6)
+,AVG(g2)
+,AVG(g6)avg_g6
+-- ,CORR(g2,g6)
+,CORR(g2,g4)corr_g2g4
+-- ,MIN(ydate)
+,COUNT(ydate)ccount
+-- ,MAX(ydate)
+FROM btg12
+-- WHERE ABS(rscore_diff1)IN(0.7,0.8,0.9)
+WHERE ABS(rscore_diff1)>0.6
+AND SIGN(g2) != 0
+AND ABS(g2) > 0.0006
+GROUP BY rscore_diff1,(SIGN(g2) * SIGN(score_diff))
+ORDER BY rscore_diff1,(SIGN(g2) * SIGN(score_diff))
 /
 
-ANALYZE TABLE scc16_t COMPUTE STATISTICS;
-
-DROP TABLE scc18_t;
-CREATE TABLE scc18_t COMPRESS AS
 SELECT
-s.pair
-,s.ydate
-,cpp1
-,cpp2
-,cpp3
-,crr1
-,crr2
-,crr3
-FROM scc16_t s
-,(SELECT pair,MAX(ydate)ydate FROM scc16_t GROUP BY pair) y
-WHERE s.pair= y.pair
-AND s.ydate = y.ydate
-/
-
-ANALYZE TABLE scc18_t COMPUTE STATISTICS;
-
--- rpt
-SELECT * FROM scc18_t;
-
-
-CREATE OR REPLACE VIEW scc20 AS
-SELECT * FROM scc18_t WHERE(cpp1>0 AND crr1>0)
-UNION
-SELECT * FROM scc18_t WHERE(cpp2>0 AND crr2>0)
-UNION
-SELECT * FROM scc18_t WHERE(cpp3>0 AND crr3>0)
-UNION
-SELECT * FROM scc18_t WHERE(cpp1<0 AND crr1<0)
-UNION
-SELECT * FROM scc18_t WHERE(cpp2<0 AND crr2<0)
-UNION
-SELECT * FROM scc18_t WHERE(cpp3<0 AND crr3<0)
-/
-
--- rpt
-SELECT * FROM scc20
-ORDER BY cpp1
+SIGN(g2) * SIGN(score_diff) sprod
+,rscore_diff1
+,AVG(g4)avg_g4
+,CORR(score_diff,g6)
+,AVG(g2)
+,AVG(g6)avg_g6
+-- ,CORR(g2,g6)
+,CORR(g2,g4)corr_g2g4
+-- ,MIN(ydate)
+,COUNT(ydate)ccount
+-- ,MAX(ydate)
+FROM btg12
+-- WHERE ABS(rscore_diff1)IN(0.7,0.8,0.9)
+WHERE ABS(rscore_diff1)>0.6
+AND SIGN(g2) != 0
+GROUP BY rscore_diff1,(SIGN(g2) * SIGN(score_diff))
+ORDER BY rscore_diff1,(SIGN(g2) * SIGN(score_diff))
 /
 
 exit
