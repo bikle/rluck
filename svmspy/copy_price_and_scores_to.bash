0ff2eb3c7d109584a88bfca075a9e02e5dd48041
Dan Bikle <bikle@bikle.com>
2011-03-27 20:51:31 +0100

    svmspy/copy_price_and_scores_to.bash

--- svmspy/copy_price_and_scores_to.bash
+++ svmspy/copy_price_and_scores_to.bash
@@ -2,14 +2,10 @@
 
 # copy_price_and_scores_to.bash
 
-
 # I use this script to copy prices and scores from point-a to point-b
-
-set -x
 
 cd /pt/s/rluck/svmspy/ibapi/
 
-set -x
 export myts=`date +%Y_%m_%d_%H_%M`
 
 expdp trade/t dumpfile=svmspy_di5.${myts}.dpdmp tables=di5min_stk0,di5min_stk,ibs5min,ibs_old,ibs_dups_old,ibs_stage,di5min_stk_c2,stkscores
@@ -23,5 +19,8 @@ echo "scp -p /oracle/app/oracle/admin/orcl/dpdump/svmspy_di5.${myts}.dpdmp z3:/o
 
 echo After scp, do ssh, then:
 
-echo "impdp trade/t table_exists_action=replace dumpfile=svmspy_di5.${myts}.dpdmp"
+echo "impdp trade/t table_exists_action=replace dumpfile=svmspy_di5.${myts}.dpdmp tables=di5min_stk_c2,ibs_stage"
+echo "impdp trade/t table_exists_action=append dumpfile=svmspy_di5.${myts}.dpdmp tables=ibs5min,stkscores"
+echo "sqt @de_dup_stkscores"
+echo "sqt @ibapi/merge"
 
