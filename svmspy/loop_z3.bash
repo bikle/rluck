a3c56b6841a6a0b4f8e25f67c082c684cb53f5e1
Dan Bikle <bikle@bikle.com>
2011-04-11 13:52:45 +0100

    loop_z3.bash

--- svmspy/loop_z3.bash
+++ svmspy/loop_z3.bash
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# loop_some.bash
-
 . /pt/s/rluck/svmspy/.orcl
 . /pt/s/rluck/svmspy/.jruby
 
@@ -11,30 +9,97 @@ date
 
 cd $SVMSPY
 
-./dl_then_svm.bash NEM
-./dl_then_svm.bash NFLX
-./dl_then_svm.bash ORCL
-./dl_then_svm.bash PALL
-./dl_then_svm.bash PBR
-./dl_then_svm.bash PM
-./dl_then_svm.bash POT
-./dl_then_svm.bash PPLT
-./dl_then_svm.bash QQQ
-./dl_then_svm.bash RIMM
-./dl_then_svm.bash SJM
-./dl_then_svm.bash SKX
-./dl_then_svm.bash SLB
-./dl_then_svm.bash SLV
-./dl_then_svm.bash SNDK
-./dl_then_svm.bash SPY
-./dl_then_svm.bash SVM
-./dl_then_svm.bash SWC
-./dl_then_svm.bash TLT
-./dl_then_svm.bash V
-./dl_then_svm.bash VECO
-./dl_then_svm.bash VMW
-./dl_then_svm.bash WDC
-./dl_then_svm.bash WMT
-./dl_then_svm.bash XLB
-./dl_then_svm.bash XLE
-./dl_then_svm.bash XOM
+# aapl has bad data: ./dl_then_svm.bash AAPL
+
+./dl_then_svm.bash MOS     
+./dl_then_svm.bash MVG     
+./dl_then_svm.bash NEM     
+./dl_then_svm.bash NFLX    
+./dl_then_svm.bash ORCL    
+./dl_then_svm.bash PALL    
+./dl_then_svm.bash PBR     
+./dl_then_svm.bash PM      
+./dl_then_svm.bash POT     
+./dl_then_svm.bash PPLT    
+./dl_then_svm.bash QCOM    
+./dl_then_svm.bash QQQ     
+./dl_then_svm.bash RIMM    
+./dl_then_svm.bash SJM     
+./dl_then_svm.bash SKX     
+./dl_then_svm.bash SLB     
+./dl_then_svm.bash SLV     
+./dl_then_svm.bash SNDK    
+./dl_then_svm.bash SPY     
+./dl_then_svm.bash SVM     
+./dl_then_svm.bash SWC     
+./dl_then_svm.bash T       
+./dl_then_svm.bash TKR     
+./dl_then_svm.bash TLT     
+./dl_then_svm.bash TSO     
+./dl_then_svm.bash UNH     
+./dl_then_svm.bash UNP     
+./dl_then_svm.bash UPS     
+./dl_then_svm.bash V       
+./dl_then_svm.bash VALE    
+./dl_then_svm.bash VECO    
+./dl_then_svm.bash VLO     
+./dl_then_svm.bash VMW     
+./dl_then_svm.bash WDC     
+./dl_then_svm.bash WFMI    
+./dl_then_svm.bash WHR     
+./dl_then_svm.bash WMT     
+./dl_then_svm.bash WYNN    
+./dl_then_svm.bash X       
+./dl_then_svm.bash XLB     
+./dl_then_svm.bash XLE     
+./dl_then_svm.bash XLU     
+./dl_then_svm.bash XOM     
+
+exit
+
+./dl_then_svm.bash ABT     
+./dl_then_svm.bash ABX     
+./dl_then_svm.bash ADBE    
+./dl_then_svm.bash AEM     
+./dl_then_svm.bash AFL     
+./dl_then_svm.bash AGU     
+./dl_then_svm.bash AIG     
+./dl_then_svm.bash AKAM    
+./dl_then_svm.bash ALL     
+./dl_then_svm.bash AMGN    
+./dl_then_svm.bash AMT     
+./dl_then_svm.bash AMX     
+./dl_then_svm.bash AMZN    
+./dl_then_svm.bash APA     
+./dl_then_svm.bash APC     
+./dl_then_svm.bash ARG     
+./dl_then_svm.bash AXU     
+./dl_then_svm.bash BA      
+./dl_then_svm.bash BBT     
+./dl_then_svm.bash BBY     
+./dl_then_svm.bash BHP     
+./dl_then_svm.bash BIDU    
+./dl_then_svm.bash BRCM    
+./dl_then_svm.bash BTU     
+./dl_then_svm.bash COF     
+./dl_then_svm.bash CVE     
+./dl_then_svm.bash DIA     
+./dl_then_svm.bash DIS     
+./dl_then_svm.bash DTV     
+./dl_then_svm.bash EBAY    
+./dl_then_svm.bash EFA     
+./dl_then_svm.bash EXK     
+./dl_then_svm.bash FFIV    
+./dl_then_svm.bash FSLR    
+./dl_then_svm.bash GDX     
+./dl_then_svm.bash GFI     
+./dl_then_svm.bash GLD     
+./dl_then_svm.bash GOOG    
+./dl_then_svm.bash GS      
+./dl_then_svm.bash HPQ     
+./dl_then_svm.bash IBM     
+./dl_then_svm.bash IOC     
+./dl_then_svm.bash IWM     
+./dl_then_svm.bash KO      
+./dl_then_svm.bash LFT     
+./dl_then_svm.bash MJN     
--- svmspy/qry_recent_stkscores.sql
+++ svmspy/qry_recent_stkscores.sql
@@ -4,7 +4,7 @@
 
 -- I use this script to see which tkrdates have been scored recently.
 
-SELECT tkr,COUNT(tkr)FROM stkscores WHERE ydate > '2011-04-08'GROUP BY tkr ORDER BY tkr;
+SELECT tkr,COUNT(tkr)FROM stkscores WHERE ydate > '2011-04-04'GROUP BY tkr ORDER BY tkr;
 
 SELECT * FROM stkscores WHERE rundate> sysdate -1/24 ORDER BY rundate;
 
