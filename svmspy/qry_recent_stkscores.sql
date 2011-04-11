eadd6e2a967bc1a6ba13576c4e2e1f2a89aa5021
Dan Bikle <bikle@bikle.com>
2011-04-11 10:42:59 +0100

    svmspy/qrs.sql

--- svmspy/qry_recent_stkscores.sql
+++ svmspy/qry_recent_stkscores.sql
@@ -4,6 +4,8 @@
 
 -- I use this script to see which tkrdates have been scored recently.
 
+SELECT tkr,COUNT(tkr)FROM stkscores WHERE ydate > '2011-04-08'GROUP BY tkr ORDER BY tkr;
+
 SELECT * FROM stkscores WHERE rundate> sysdate -1/24 ORDER BY rundate;
 
 exit
