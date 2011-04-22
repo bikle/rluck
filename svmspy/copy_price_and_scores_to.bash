044d9110eab08dc5152804d3f2cfe49d6c6b08ab
Dan Bikle <bikle@bikle.com>
2011-04-22 18:25:36 +0100

    svmspy/copy_price_and_scores_to.bash

--- svmspy/copy_price_and_scores_to.bash
+++ svmspy/copy_price_and_scores_to.bash
@@ -10,12 +10,12 @@ export myts=`date +%Y_%m_%d_%H_%M`
 
 expdp trade/t dumpfile=svmspy_di5.${myts}.dpdmp tables=di5min_stk0,di5min_stk,ibs5min,ibs_old,ibs_dups_old,ibs_stage,di5min_stk_c2,stkscores
 
-echo "scp -p /oracle/app/oracle/admin/orcl/dpdump/svmspy_di5.${myts}.dpdmp usr10@xp:dpdump/"
-echo "scp -p /oracle/app/oracle/admin/orcl/dpdump/svmspy_di5.${myts}.dpdmp l:/oracle/app/oracle/admin/orcl/dpdump/"
-echo "scp -p /oracle/app/oracle/admin/orcl/dpdump/svmspy_di5.${myts}.dpdmp h:/oracle/app/oracle/admin/orcl/dpdump/"
-echo "scp -p /oracle/app/oracle/admin/orcl/dpdump/svmspy_di5.${myts}.dpdmp z:/oracle/app/oracle/admin/orcl/dpdump/"
-echo "scp -p /oracle/app/oracle/admin/orcl/dpdump/svmspy_di5.${myts}.dpdmp z2:/oracle/app/oracle/admin/orcl/dpdump/"
-echo "scp -p /oracle/app/oracle/admin/orcl/dpdump/svmspy_di5.${myts}.dpdmp z3:/oracle/app/oracle/admin/orcl/dpdump/"
+echo "scp -p ~/dpdump/svmspy_di5.${myts}.dpdmp usr10@xp:dpdump/"
+echo "scp -p ~/dpdump/svmspy_di5.${myts}.dpdmp h:~/dpdump/"
+echo "scp -p ~/dpdump/svmspy_di5.${myts}.dpdmp h2:~/dpdump/"
+echo "scp -p ~/dpdump/svmspy_di5.${myts}.dpdmp z:~/dpdump/"
+echo "scp -p ~/dpdump/svmspy_di5.${myts}.dpdmp z2:~/dpdump/"
+echo "scp -p ~/dpdump/svmspy_di5.${myts}.dpdmp z3:~/dpdump/"
 
 echo After scp, do ssh, then:
 
